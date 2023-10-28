543f42fb621e607970ccefb90dc7b0c871c0c3af
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2023-10-28 15:39:51 +0200

    [feat] pages added with basic prototype

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./page/home/home.component";
+import {DashboardComponent} from "./page/dashboard/dashboard.component";
+import {SearchComponent} from "./page/search/search.component";
+import {GalleryComponent} from "./page/gallery/gallery.component";
+import {WorkspaceComponent} from "./page/workspace/workspace.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'workspace', component: WorkspaceComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', redirectTo: "/home"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
