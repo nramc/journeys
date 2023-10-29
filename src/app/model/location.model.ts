cdc2dc64e15f08783fcaa1356dc4744fcffc23ca
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2023-10-29 22:40:26 +0100

    [feat] location model added

---
+++ src/app/model/location.model.ts
@@ -0,0 +1,10 @@
+export interface Location {
+  "name": string,
+  "type": string,
+  "latitude": number,
+  "longitude": number,
+  "city": string,
+  "country": string,
+  "address"?: string,
+  "tags": string[]
+}
