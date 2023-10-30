f4560dbeb8a53b49c2ec3b490810f2cae4818094
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2023-10-30 12:53:44 +0100

    [chore] minor refactoring

--- src/app/component/world-map/world-map.component.ts
+++ src/app/component/world-map/world-map.component.ts
@@ -29,8 +29,11 @@ export class WorldMapComponent implements AfterViewInit {
   constructor(private locationService: LocationService) {
   }
 
+  ngAfterViewInit(): void {
+    this.initializeMap();
+  }
 
-  private initMap(): void {
+  private initializeMap(): void {
 
     this.map = L.map('map').fitWorld();
 
@@ -38,7 +41,7 @@ export class WorldMapComponent implements AfterViewInit {
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
 
     this.locationService.getAllAvailableLocations()
@@ -60,8 +63,6 @@ export class WorldMapComponent implements AfterViewInit {
       .addTo(map);
   }
 
-  ngAfterViewInit(): void {
-    this.initMap();
-  }
+
 
 }
