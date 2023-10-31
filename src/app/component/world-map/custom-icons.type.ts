7633cb705b36c9dc811a15acf7f338a3b5db6491
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2023-10-31 20:45:03 +0100

    [feat] marker icon hover effect added

--- src/app/component/world-map/custom-icons.type.ts
+++ src/app/component/world-map/custom-icons.type.ts
@@ -2,7 +2,7 @@ import * as L from "leaflet";
 
 export const iconHome = L.divIcon({
   className: 'custom-div-icon',
-  html: "<div class='marker-pin'></div><i class='material-icons text-primary fs-5'>home</i>",
+  html: "<div class='marker-icon-wrapper'><div class='marker-pin'></div><i class='material-icons text-primary fs-5'>home</i></div>",
   iconSize: [30, 42],
   iconAnchor: [15, 42],
   popupAnchor: [1, -34],
@@ -13,7 +13,7 @@ export const iconHome = L.divIcon({
 
 export const iconVisited = L.divIcon({
   className: 'custom-div-icon',
-  html: "<div class='marker-pin'></div><i class='material-icons text-primary fs-5'>check</i>",
+  html: "<div class='marker-icon-wrapper'><div class='marker-pin'></div><i class='material-icons text-primary fs-5'>check</i></div>",
   iconSize: [30, 42],
   iconAnchor: [15, 42],
   popupAnchor: [1, -34],
