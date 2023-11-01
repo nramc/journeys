da89e51c0d91f824f696999b6da56ac9d0c54aa9
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2023-11-01 13:21:39 +0100

    [feat] location component enhanced with received data by path param

--- src/app/page/location/location.component.ts
+++ src/app/page/location/location.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PageInfo} from "../../model/page-info";
+import {LocationService} from "../../service/location.service";
+import {ActivatedRoute} from "@angular/router";
+import {Feature} from "geojson";
 
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.scss']
 })
-export class LocationComponent {
+export class LocationComponent implements OnInit {
+  defaultDescription: string = 'Remember that happiness is a way of travel, not a destination.';
+  locationInfo: PageInfo | undefined;
+  location: Feature | undefined;
 
+  constructor(private locationService: LocationService,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    const locationId = this.route.snapshot.params['id'];
+    console.log(locationId)
+    this.locationService.getLocationById(locationId)
+      .subscribe(data => data ? this.dataReceivedCallback(data) : console.warn("Location not exists"));
+
+  }
+
+  private dataReceivedCallback(data: Feature) {
+    this.location = data;
+    this.locationInfo = {
+      title: data?.properties?.['name'],
+      name: data?.properties?.['name'],
+      description: data?.properties?.['description'] || this.defaultDescription,
+      path: "/place/" + data?.id
+    };
+  }
 }
