9070fd56675f5cb287d48fba4f4413c2ebb1361d
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2023-11-13 20:11:59 +0100

    40 feature location view enhance with map (#65)
    * [feature] geojson type updated to GeoJsonObject to use both feature and feature object
    
    * [feature] popup to marker added conditionally
    
    * [feature] location type adopted to add geojson feature
    
    * [feature] map added to location component
    
    * [feature] map centering utility added
    
    * [fix] map already initialised issue fixed by injection angular ElementRef
    
    * [chore] margin top added
    
    ---------
    
    Co-authored-by: Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>

--- src/app/component/world-map/world-map.component.ts
+++ src/app/component/world-map/world-map.component.ts
@@ -1,103 +1,121 @@
-import {AfterViewInit, Component, EventEmitter, Input, Output, ViewChild, ViewContainerRef} from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    EventEmitter,
+    inject,
+    Input,
+    Output,
+    ViewChild,
+    ViewContainerRef
+} from '@angular/core';
 import * as L from 'leaflet';
 import {Layer} from 'leaflet';
 import {MarkerPopupComponent} from "../marker-popup/marker-popup.component";
-import {Feature, FeatureCollection} from "geojson";
+import {Feature, GeoJsonObject} from "geojson";
 import {iconHome} from "./custom-icons.type";
 
 
 const iconDefault = L.icon({
-  iconRetinaUrl: 'assets/marker-icon-2x.png',
-  iconUrl: 'assets/marker-icon.png',
-  shadowUrl: 'assets/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
+    iconRetinaUrl: 'assets/marker-icon-2x.png',
+    iconUrl: 'assets/marker-icon.png',
+    shadowUrl: 'assets/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
 });
 L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
-  selector: 'app-world-map',
-  templateUrl: './world-map.component.html',
-  styleUrls: ['./world-map.component.scss']
+    selector: 'app-world-map',
+    templateUrl: './world-map.component.html',
+    styleUrls: ['./world-map.component.scss']
 })
 export class WorldMapComponent implements AfterViewInit {
+    private elementRef: ElementRef = inject(ElementRef);
+    private map: L.Map | undefined;
+    private geoJsonLayer: L.GeoJSON | undefined;
 
-  private map: L.Map | undefined;
-  private geoJsonLayer: L.GeoJSON | undefined;
+    @Output() mapInitializedEvent = new EventEmitter<L.Map>();
+    @ViewChild("markerPopupViewContainer", {read: ViewContainerRef}) markerPopupViewContainerRef: ViewContainerRef | undefined;
 
-  @Output() mapInitializedEvent = new EventEmitter<L.Map>();
-  @ViewChild("markerPopupViewContainer", {read: ViewContainerRef}) markerPopupViewContainerRef: ViewContainerRef | undefined;
+    #featureCollection: GeoJsonObject | undefined;
 
-  #featureCollection: FeatureCollection | undefined;
-
-  @Input("geoJsonData")
-  set geoJsonData(geoJsonData: FeatureCollection | undefined) {
-    this.#featureCollection = geoJsonData;
-    this.addGeoJsonData(geoJsonData);
-  }
-
-
-  ngAfterViewInit(): void {
-    this.initializeMap();
-    this.mapInitializedEvent.emit(this.map);
-  }
-
-  private initializeMap(): void {
-
-    this.map = L.map('map').fitWorld();
-
-    L.control.scale().addTo(this.map);
-
-    this.addTileLayers();
-    this.addGeoJsonLayer();
-  }
-
-  private addTileLayers() {
-    if (this.map) {
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      }).addTo(this.map);
-    }
-  }
-
-  private addGeoJsonData(geoJsonData: FeatureCollection | undefined) {
-    if (geoJsonData) {
-      this.geoJsonLayer?.addData(geoJsonData);
-    } else {
-      this.geoJsonLayer?.clearLayers();
-    }
-  }
-
-  private addGeoJsonLayer() {
-
-    const getPopupComponentNativeElement = (feature: Feature) => {
-      let popupComponent = this.markerPopupViewContainerRef?.createComponent(MarkerPopupComponent);
-      popupComponent!.instance.feature = feature;
-      return popupComponent?.instance.elementRef.nativeElement;
+    @Input("geoJsonData")
+    set geoJsonData(geoJsonData: GeoJsonObject | undefined) {
+        this.#featureCollection = geoJsonData;
+        this.addGeoJsonData(geoJsonData);
     }
 
-    if (this.map) {
+    @Input() disablePopup: boolean = false;
 
-      this.geoJsonLayer = L.geoJSON(this.#featureCollection, {
-        pointToLayer: (feature, latlng) => {
-          return L.marker(latlng, {
-            icon: iconHome,
-          })
-        },
-        onEachFeature: function (feature: Feature, layer: Layer) {
-          layer.bindTooltip(feature.properties?.['name']);
-          layer.bindPopup(getPopupComponentNativeElement(feature))
+
+    ngAfterViewInit(): void {
+        this.initializeMap();
+        this.mapInitializedEvent.emit(this.map);
+    }
+
+    private initializeMap(): void {
+
+        this.map = L.map(this.elementRef.nativeElement.querySelector("div.map-renderer")).fitWorld();
+
+        L.control.scale().addTo(this.map);
+
+        this.addTileLayers();
+        this.addGeoJsonLayer();
+    }
+
+    private addTileLayers() {
+        if (this.map) {
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(this.map);
         }
-      })
-        .addTo(this.map);
-
-    } else {
-      console.warn("Unable to create GeoJson Layer. Cause:Map is not yet initialised")
     }
-  }
+
+    private addGeoJsonData(geoJsonData: GeoJsonObject | undefined) {
+        if (geoJsonData) {
+            this.geoJsonLayer?.addData(geoJsonData);
+        } else {
+            this.geoJsonLayer?.clearLayers();
+        }
+    }
+
+    private addGeoJsonLayer() {
+
+        const getPopupComponentNativeElement = (feature: Feature) => {
+            let popupComponent = this.markerPopupViewContainerRef?.createComponent(MarkerPopupComponent);
+            popupComponent!.instance.feature = feature;
+            return popupComponent?.instance.elementRef.nativeElement;
+        }
+
+        const bindPopupIfRequired = (layer: Layer, feature: Feature) => {
+            if (!this.disablePopup) {
+                layer.bindTooltip(feature.properties?.['name']);
+                layer.bindPopup(getPopupComponentNativeElement(feature))
+            }
+        }
+
+        if (this.map) {
+
+            this.geoJsonLayer = L.geoJSON(this.#featureCollection, {
+                pointToLayer: (feature, latlng) => {
+                    return L.marker(latlng, {
+                        icon: iconHome,
+                    })
+                },
+                onEachFeature: function (feature: Feature, layer: Layer) {
+                    bindPopupIfRequired(layer, feature);
+                }
+            })
+                .addTo(this.map);
+
+        } else {
+            console.warn("Unable to create GeoJson Layer. Cause:Map is not yet initialised")
+        }
+    }
 
 }
--- src/app/page/location/location.component.ts
+++ src/app/page/location/location.component.ts
@@ -6,53 +6,59 @@ import {Geometry} from "geojson";
 import {Location} from "../../model/location.model";
 import {convertFeatureToLocation} from "../../utility/feature-to-location.converter";
 import {map, Observable} from "rxjs";
+import * as L from "leaflet";
+import {getLatLangToFocus} from "../../utility/map.utility";
 
 @Component({
-  selector: 'app-location',
-  templateUrl: './location.component.html',
-  styleUrls: ['./location.component.scss']
+    selector: 'app-location',
+    templateUrl: './location.component.html',
+    styleUrls: ['./location.component.scss']
 })
 export class LocationComponent implements OnInit {
-  defaultDescription: string = 'Remember that happiness is a way of travel, not a destination.';
+    defaultDescription: string = 'Remember that happiness is a way of travel, not a destination.';
 
-  location$: Observable<Location<Geometry> | undefined> | undefined;
+    location$: Observable<Location<Geometry> | undefined> | undefined;
 
-  constructor(private locationService: LocationService,
-              private route: ActivatedRoute) {
-  }
-
-  ngOnInit(): void {
-    const locationId = this.route.snapshot.params['id'];
-    console.log(locationId);
-
-    this.location$ = this.locationService.getLocationById(locationId)
-      .pipe(map(data => data ? convertFeatureToLocation(data) : undefined));
-
-
-  }
-
-  getPageInfo(location: Location<Geometry>): PageInfo {
-    return {
-      title: location.name,
-      name: location.name,
-      description: location?.rawProperties?.['description'] || this.defaultDescription,
-      path: "/place/" + location?.id
-    };
-  }
-
-  getImages(location: Location<Geometry>): string[] {
-    let images = new Array<string>()
-    for (let eventEntry of location.events) {
-      eventEntry.images?.map(img => images.push(img));
+    constructor(private locationService: LocationService,
+                private route: ActivatedRoute) {
     }
-    return images;
-  }
 
-  getVideos(location: Location<Geometry>): string[] {
-    let videos = new Array<string>()
-    for (let eventEntry of location.events) {
-      eventEntry.videos?.map(vid => videos.push(vid));
+    ngOnInit(): void {
+        const locationId = this.route.snapshot.params['id'];
+        console.log(locationId);
+
+        this.location$ = this.locationService.getLocationById(locationId)
+            .pipe(map(data => data ? convertFeatureToLocation(data) : undefined));
+
+
     }
-    return videos;
-  }
+
+    getPageInfo(location: Location<Geometry>): PageInfo {
+        return {
+            title: location.name,
+            name: location.name,
+            description: location?.rawProperties?.['description'] || this.defaultDescription,
+            path: "/place/" + location?.id
+        };
+    }
+
+    getImages(location: Location<Geometry>): string[] {
+        let images = new Array<string>()
+        for (let eventEntry of location.events) {
+            eventEntry.images?.map(img => images.push(img));
+        }
+        return images;
+    }
+
+    getVideos(location: Location<Geometry>): string[] {
+        let videos = new Array<string>()
+        for (let eventEntry of location.events) {
+            eventEntry.videos?.map(vid => videos.push(vid));
+        }
+        return videos;
+    }
+
+    onMapInitializationComplete(map: L.Map, location: Location<Geometry>) {
+        map.setView(getLatLangToFocus(location.geometry), 10);
+    }
 }
