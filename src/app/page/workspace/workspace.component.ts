9b46df227a769ef03a3749042cd0622dbf98d748
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-03-24 20:20:45 +0100

    [chore] angular env setup added

--- src/app/page/workspace/workspace.component.ts
+++ src/app/page/workspace/workspace.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {WORKSPACE_PAGE_INFO} from "../../model/page-info";
-import {Cloudinary, CloudinaryImage} from "@cloudinary/url-gen";
-import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-workspace',
@@ -10,18 +8,8 @@ import {environment} from "../../../environments/environment";
 })
 export class WorkspaceComponent implements OnInit {
   protected readonly WORKSPACE_PAGE_INFO = WORKSPACE_PAGE_INFO;
-  imageSample!: CloudinaryImage;
 
   ngOnInit(): void {
-    const cloundinary = new Cloudinary({
-      cloud: {
-        cloudName: environment.cloudName
-      }
-    });
-
-    console.log("Cloud Name:", environment.cloudName)
-    // sample
-    this.imageSample = cloundinary.image("sample");
 
   }
 
