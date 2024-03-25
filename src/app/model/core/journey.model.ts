07668a995247b4ccf42049235f658ff5de4a557e
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-03-25 20:17:45 +0100

    [feat] #76 basic form design and validation added for journey

--- src/app/model/core/journey.model.ts
+++ src/app/model/core/journey.model.ts
@@ -1,11 +1,21 @@
+import {Geometry} from "geojson";
+
 export class Journey {
   constructor(
+    public id: string = '',
     public name: string = '',
     public title: string = '',
-    public category: string = '',
     public description: string = '',
     public city: string = '',
-    public country: string = ''
+    public country: string = '',
+    public category: string = '',
+    public tags: string[] = ['travel', 'initial'],
+    public thumbnail: string = 'default',
+    public location: Geometry = {
+      type: "Point",
+      coordinates: [48.183160038296585, 11.53090747669896]
+
+    }
   ) {
   }
 }
--- src/app/page/workspace/workspace.component.ts
+++ src/app/page/workspace/workspace.component.ts
@@ -1,25 +1,47 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {WORKSPACE_PAGE_INFO} from "../../model/page-info";
 import {Journey} from "../../model/core/journey.model";
 import {NgForm} from "@angular/forms";
+import {JourneyService} from "../../service/journey.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-workspace',
   templateUrl: './workspace.component.html',
   styleUrls: ['./workspace.component.scss']
 })
-export class WorkspaceComponent implements OnInit {
+export class WorkspaceComponent {
   protected readonly WORKSPACE_PAGE_INFO = WORKSPACE_PAGE_INFO;
 
+  constructor(private journeyService: JourneyService, private snackBar: MatSnackBar) {
+  }
+
   journey: Journey = new Journey();
+  successMessage: string = '';
+  errorMessage: string = '';
 
-  ngOnInit(): void {
+  setSuccess(message: string) {
+    this.successMessage = message;
+    this.errorMessage = '';
+  }
 
+  setError(message: string) {
+    this.successMessage = '';
+    this.errorMessage = message;
   }
 
   onSubmit(journeyForm: NgForm) {
     if (journeyForm.submitted && journeyForm.valid) {
-      console.log(this.journey);
+      this.journeyService.createJourney(this.journey)
+        .subscribe(data => this.onCreateSuccess(data));
+    } else {
+      console.log(journeyForm)
     }
   }
+
+  onCreateSuccess(result: Journey) {
+    this.setSuccess('Journey added successfully.');
+    this.journey = result;
+  }
+
 }
