9992cb1113f135b0ff9aa8fa358862e27a629bbf
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-03-28 12:13:36 +0100

    [feat] #83 Create New Journey page integrated into GeoPin page
     - journey creation workflow adopted
     - new date adopter added for date customization
     - navigation added to edit page
     - edit page adopted with fetching journey data

--- src/app/page/journeys/edit-journey/edit-journey.component.ts
+++ src/app/page/journeys/edit-journey/edit-journey.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 import {JourneyService} from "../../../service/journey/journey.service";
-import {Observable, switchMap} from "rxjs";
+import {switchMap} from "rxjs";
 import {Journey} from "../../../model/core/journey.model";
 
 @Component({
@@ -9,7 +9,7 @@ import {Journey} from "../../../model/core/journey.model";
   templateUrl: './edit-journey.component.html'
 })
 export class EditJourneyComponent implements OnInit {
-  journey$: Observable<Journey> | undefined;
+  journey!: Journey;
 
   constructor(
     private route: ActivatedRoute,
@@ -19,10 +19,13 @@ export class EditJourneyComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.journey$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) =>
-        this.journeyService.getJourneyById(params.get('id')!))
-    );
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.journeyService.getJourneyById(params.get('id')!))
+    )
+      .subscribe({
+        next: data => this.journey = data,
+        error: err => console.error(err)
+      });
   }
 
 }
