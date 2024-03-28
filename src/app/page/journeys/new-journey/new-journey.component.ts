471cf020ec2ec7984fc1f99fc8db55302bd7a27c
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2024-03-28 16:11:56 +0100

    85 feature implement tags (#87)
    * [feat] #85 compile error fixed
    
     - old journey create workflow removed
    
    * [feat] #85 compile error fixed
    
     - input field added for tags
    
    * [feat] #85 compile error fixed
    
     - category auto suggestion added
    
    ---------
    
    Co-authored-by: Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>

--- src/app/page/journeys/new-journey/new-journey.component.ts
+++ src/app/page/journeys/new-journey/new-journey.component.ts
@@ -4,6 +4,9 @@ import {Journey} from "../../../model/core/journey.model";
 import {JourneyService} from "../../../service/journey/journey.service";
 import {NgForm} from "@angular/forms";
 import {Router} from "@angular/router";
+import {COMMA, ENTER, SPACE} from "@angular/cdk/keycodes";
+import {MatChipInputEvent} from "@angular/material/chips";
+import {debounceTime, distinctUntilChanged, map, Observable, OperatorFunction} from "rxjs";
 
 @Component({
   selector: 'app-new-journey',
@@ -11,6 +14,8 @@ import {Router} from "@angular/router";
 })
 export class NewJourneyComponent {
   protected readonly NEW_JOURNEY_PAGE_INFO = NEW_JOURNEY_PAGE_INFO;
+  readonly separatorKeysCodes = [ENTER, COMMA, SPACE] as const;
+  readonly predefinedCategories = ['Travel', 'Work', 'Residential']
 
   journey: Journey = new Journey();
 
@@ -55,4 +60,30 @@ export class NewJourneyComponent {
       this.router.navigate(['/journey', this.journey.id, 'edit']);
     }
   }
+
+  addTag(event: MatChipInputEvent): void {
+    const newTag = (event.value || '').trim();
+    if (newTag) {
+      this.journey.tags.push(newTag);
+    }
+    // Clear the input value
+    event.chipInput.clear();
+  }
+
+  removeTag(tag: string): void {
+    const index = this.journey.tags.indexOf(tag);
+    if (index >= 0) {
+      this.journey.tags.splice(index, 1);
+    }
+  }
+
+  searchCategory: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map((term) =>
+        term.length < 2 ? [] :
+          this.predefinedCategories.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10)
+      ),
+    );
 }
