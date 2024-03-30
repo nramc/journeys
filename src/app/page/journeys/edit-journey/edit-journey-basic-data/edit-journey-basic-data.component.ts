2f9c4c2da116a953a56687c7485e87fb7f559dc2
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-03-30 19:47:41 +0100

    [feat] #90 Edit Journey details
     - new component added to edit Journey basic data

---
+++ src/app/page/journeys/edit-journey/edit-journey-basic-data/edit-journey-basic-data.component.ts
@@ -0,0 +1,107 @@
+import {Component, OnInit} from '@angular/core';
+import {COMMA, ENTER, SPACE} from "@angular/cdk/keycodes";
+import {Journey} from "../../../../model/core/journey.model";
+import {ActivatedRoute, ParamMap, Router} from "@angular/router";
+import {JourneyService} from "../../../../service/journey/journey.service";
+import {debounceTime, distinctUntilChanged, map, Observable, OperatorFunction, switchMap} from "rxjs";
+import {Point} from "geojson";
+import {MatChipInputEvent} from "@angular/material/chips";
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-edit-journey-basic-data',
+  templateUrl: './edit-journey-basic-data.component.html',
+  styleUrl: './edit-journey-basic-data.component.scss'
+})
+export class EditJourneyBasicDataComponent implements OnInit {
+  readonly separatorKeysCodes = [ENTER, COMMA, SPACE] as const;
+  readonly predefinedCategories = ['Travel', 'Work', 'Residential']
+
+  successMessage: string = '';
+  errorMessage: string = '';
+
+  setSuccess(message: string) {
+    this.successMessage = message;
+    this.errorMessage = '';
+  }
+
+  setError(message: string) {
+    this.successMessage = '';
+    this.errorMessage = message;
+  }
+
+  journey!: Journey;
+  coordinates: number[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private journeyService: JourneyService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.journeyService.getJourneyById(params.get('id')!))
+    )
+      .subscribe({
+        next: data => this.onFetchSuccess(data),
+        error: err => console.error(err)
+      });
+  }
+
+  onFetchSuccess(journey: Journey) {
+    this.journey = journey;
+    this.coordinates = (journey.location as Point).coordinates;
+  }
+
+  onUpdateSuccess(result: Journey) {
+    this.setSuccess('Journey saved successfully.');
+    this.journey = result;
+  }
+
+  continue() {
+    if (this.journey.id) {
+      this.router.navigate(['/journey', this.journey.id, 'edit']);
+    }
+  }
+
+  addTag(event: MatChipInputEvent): void {
+    const newTag = (event.value || '').trim();
+    if (newTag) {
+      this.journey.tags.push(newTag);
+    }
+    // Clear the input value
+    event.chipInput.clear();
+  }
+
+  removeTag(tag: string): void {
+    const index = this.journey.tags.indexOf(tag);
+    if (index >= 0) {
+      this.journey.tags.splice(index, 1);
+    }
+  }
+
+  searchCategory: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map((term) =>
+        term.length < 2 ? [] :
+          this.predefinedCategories.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10)
+      ),
+    );
+
+  refreshMapWithCoordinates() {
+    if (this.coordinates.length == 2) {
+      this.journey.location = {
+        type: "Point",
+        coordinates: this.coordinates
+      }
+    }
+  }
+
+  save(journeyForm: NgForm) {
+    console.log('Saved', journeyForm.value);
+  }
+}
