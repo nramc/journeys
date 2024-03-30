c26fc94071a00db34a468662d5c9c9b85d9083c6
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-03-30 20:12:00 +0100

    [feat] #90 Edit Journey details
     - new component added to edit Journey basic data

--- src/app/page/journeys/edit-journey/edit-journey.component.ts
+++ src/app/page/journeys/edit-journey/edit-journey.component.ts
@@ -1,10 +1,51 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Journey} from "../../../model/core/journey.model";
+import {switchMap} from "rxjs";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {JourneyService} from "../../../service/journey/journey.service";
 
 @Component({
   selector: 'app-edit-journey',
   templateUrl: './edit-journey.component.html',
   styleUrl: './edit-journey.component.scss'
 })
-export class EditJourneyComponent {
+export class EditJourneyComponent implements OnInit {
+  journey: Journey = new Journey();
+
+  constructor(
+    private route: ActivatedRoute,
+    private journeyService: JourneyService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.fetchJourney();
+  }
+
+  fetchJourney() {
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.journeyService.getJourneyById(params.get('id')!))
+    )
+      .subscribe({
+        next: data => this.journey = data,
+        error: err => console.error(err)
+      });
+  }
+
+  isBasicDataCollected(): boolean {
+    return this.journey.id != '';
+  }
+
+  isGeoInfoCollected(): boolean {
+    return this.journey.id != '';
+  }
+
+  isEventInfoCollected(): boolean {
+    return this.journey.id != '';
+  }
+
+  isJourneyPublished(): boolean {
+    return this.journey.id != '';
+  }
 
 }
