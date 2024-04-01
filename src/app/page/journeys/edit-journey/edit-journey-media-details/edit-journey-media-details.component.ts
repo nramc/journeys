a3817723de33bd8855ac7413d742ed745836a3ee
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-04-01 20:40:28 +0200

    [feat] #74 new component added for adding media details
     - basic setup added

--- src/app/page/journeys/edit-journey/edit-journey-media-details/edit-journey-media-details.component.ts
+++ src/app/page/journeys/edit-journey/edit-journey-media-details/edit-journey-media-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Journey} from "../../../../model/core/journey.model";
+import {JourneyService} from "../../../../service/journey/journey.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-journey-media-details',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrl: './edit-journey-media-details.component.scss'
 })
 export class EditJourneyMediaDetailsComponent {
+  @Output("saved") savedEvent: EventEmitter<Journey> = new EventEmitter<Journey>();
+  @Input({required: true}) journey!: Journey;
+
+  successMessage: string = '';
+  errorMessage: string = '';
+
+  constructor(
+    private journeyService: JourneyService
+  ) {
+  }
+
+  onError(errorMessage: string, err: any) {
+    this.errorMessage = errorMessage;
+    console.error(err);
+  }
+
+  onUpdateSuccess(result: Journey) {
+    this.successMessage = 'Media Details saved successfully.';
+    this.journey = result;
+    this.savedEvent.emit(this.journey);
+  }
+
+  save(journeyForm: NgForm) {
+    console.log('Saved', journeyForm.value);
+    this.journeyService.saveJourneyBasicDetails(this.journey)
+      .subscribe({
+        next: data => this.onUpdateSuccess(data),
+        error: err => this.onError('Unexpected error while saving media data', err)
+      });
+
+  }
+
+
 
 }
