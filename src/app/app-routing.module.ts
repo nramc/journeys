72ba29b698e49612b0ebd91c87950647d55b4364
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2024-04-09 16:44:02 +0200

    [Feat] View transition added for page navigation #110 (#111)
    Co-authored-by: Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,7 +33,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableViewTransitions: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
