1150d9f2959e9477a00dc80769b18fbf77b2f4ac
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-04-12 00:50:52 +0200

    [feat] Gallery page designed and journey service adopted

--- src/app/page/gallery/gallery.component.ts
+++ src/app/page/gallery/gallery.component.ts
@@ -1,12 +1,13 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {GALLERY_PAGE_INFO} from "../../model/page-info";
-import {map, Observable} from "rxjs";
-import {Location} from "../../model/location.model";
-import {Geometry} from "geojson";
-import {LocationService} from "../../service/location.service";
-import {convertFeatureToLocation} from "../../utility/feature-to-location.converter";
+import {catchError, map, merge, of, startWith, switchMap} from "rxjs";
 import {PageHeaderComponent} from "../../component/page-header/page-header.component";
 import {AsyncPipe, JsonPipe, NgForOf, NgIf} from "@angular/common";
+import {JourneyService} from "../../service/journey/journey.service";
+import {HttpParams} from "@angular/common/http";
+import {JourneyPage} from "../../service/journey/journey-page.type";
+import {MatPaginator} from "@angular/material/paginator";
+import {Journey} from "../../model/core/journey.model";
 
 @Component({
   selector: 'app-gallery',
@@ -17,19 +18,52 @@ import {AsyncPipe, JsonPipe, NgForOf, NgIf} from "@angular/common";
     AsyncPipe,
     JsonPipe,
     NgForOf,
-    NgIf
+    NgIf,
+    MatPaginator
   ],
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit, AfterViewInit {
   protected readonly GALLERY_PAGE_INFO = GALLERY_PAGE_INFO;
-  location$: Observable<Location<Geometry>[]> | undefined;
+  isLoadingResults: boolean = false;
 
-  constructor(private locationService: LocationService) {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  defaultPageSize: number = 10;
+  totalElements: number = 0;
+  data: Journey[] = [];
+
+  constructor(private journeyService: JourneyService) {
+  }
+
+  ngAfterViewInit(): void {
+    merge(this.paginator.page)
+      .pipe(
+        startWith(),
+        switchMap(() => {
+          this.isLoadingResults = true;
+
+          return this.journeyService.findJourneyByQuery(
+            'queryString',
+            'journeyDate',
+            'desc',
+            this.paginator.pageIndex,
+            this.paginator.pageSize
+          ).pipe(catchError(() => of(null)));
+        }),
+        map(data => {
+          this.isLoadingResults = false;
+          return data;
+        }),
+      ).subscribe(data => this.onSuccess(data));
+  }
+
+  onSuccess(pageData: null | JourneyPage) {
+    this.totalElements = pageData?.totalElements || 0;
+    this.data = pageData?.content || [];
   }
 
   ngOnInit(): void {
-    this.location$ = this.locationService.getAllAvailableLocations()
-      .pipe(map(data => data ? data.features.map(convertFeatureToLocation) : []));
+    let params = new HttpParams();
+    this.journeyService.getAllJourneys(params).subscribe(data => this.onSuccess(data));
   }
 }
--- src/app/service/journey/journey.service.ts
+++ src/app/service/journey/journey.service.ts
@@ -5,6 +5,7 @@ import {environment} from "../../../environments/environment";
 import {Observable} from "rxjs";
 import {JourneyPage} from "./journey-page.type";
 import {FeatureCollection} from "geojson";
+import {SortDirection} from "@angular/material/sort";
 
 
 @Injectable({
@@ -16,6 +17,18 @@ export class JourneyService {
 
   createJourney(journey: Journey): Observable<Journey> {
     return this.httpClient.post<Journey>(environment.journeyApi + '/journey', journey);
+  }
+
+  findJourneyByQuery(queryString: string, sort: string, order: SortDirection, page: number, pageSize: number): Observable<JourneyPage> {
+    let params = new HttpParams();
+    params = params.set("q", queryString);
+    params = params.set("sort", sort);
+    params = params.set("order", order.toUpperCase());
+    params = params.set("pageIndex", page);
+    params = params.set("pageSize", pageSize);
+    console.log(params)
+
+    return this.getAllJourneys(params);
   }
 
   getAllJourneys(params: HttpParams): Observable<JourneyPage> {
