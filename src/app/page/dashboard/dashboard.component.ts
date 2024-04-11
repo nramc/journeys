e3aa5ce41295de4ecceefb75ba01947053b11668
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-04-11 12:55:58 +0200

    [feat] dashboard component updated to use journey service for fetching geo json
     - marker popup component styles improved

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -22,14 +22,37 @@ export class DashboardComponent implements OnInit {
   protected readonly DASHBOARD_PAGE_INFO = DASHBOARD_PAGE_INFO;
   // Tip: Async pipe can not be used due to popup maker component view initialisation hook
   featureCollection: FeatureCollection | undefined;
+  stats: Map<String, number> = new Map<String, number>()
 
   constructor(
     private journeyService: JourneyService) {
   }
 
   ngOnInit(): void {
+
     this.journeyService.getAllJourneysAsGeoJson()
-      .subscribe(data => this.featureCollection = data);
+      .subscribe(data => this.onFetchSuccessCallback(data));
+  }
+
+  onFetchSuccessCallback(featureCollection: FeatureCollection) {
+    this.featureCollection = featureCollection;
+    this.collectStats(featureCollection);
+  }
+
+  collectStats(featureCollection: FeatureCollection) {
+    this.stats.set("Journeys", featureCollection.features.length);
+    this.stats.set("Places", this.uniqueJourneysByProperty(featureCollection, 'location'));
+    this.stats.set("Country", this.uniqueJourneysByProperty(featureCollection, 'country'));
+    this.stats.set("City", this.uniqueJourneysByProperty(featureCollection, 'city'));
+  }
+
+  private uniqueJourneysByProperty(featureCollection: FeatureCollection, property: string) {
+    return featureCollection.features
+      .filter(feature => feature.properties?.[property])
+      .filter((feature, i, arr) =>
+        arr.findIndex(f =>
+          JSON.stringify(f.properties?.[property]) === JSON.stringify(feature.properties?.[property])) === i
+      ).length;
   }
 
 }
