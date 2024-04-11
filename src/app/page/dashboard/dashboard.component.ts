ed937b34ad2ca4f618df59b5f9255c35eb0e0b07
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2024-04-11 13:11:46 +0200

    27 feature new dashboard page to visualize all places and stats (#118)
    * [feat] dashboard component updated to use journey service for fetching geo json
    
    * [feat] dashboard component updated to use journey service for fetching geo json
    
     - disablePopup removed and isPopupRequired added
    
    * [feat] dashboard component updated to use journey service for fetching geo json
    
     - journeys fetching updated with async pipe
    
    * [feat] dashboard component updated to use journey service for fetching geo json
    
     - journeys fetching updated with async pipe
    
    * [feat] dashboard component updated to use journey service for fetching geo json
    
     - marker popup component styles improved
    
    * [feat] dashboard component updated to use journey service for fetching geo json
    
     - marker popup component styles improved
    
    ---------
    
    Co-authored-by: Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DASHBOARD_PAGE_INFO} from "../../model/page-info";
-import * as L from "leaflet";
-import {LocationService} from "../../service/location.service";
 import {WorldMapComponent} from "../../component/world-map/world-map.component";
 import {FeatureCollection} from "geojson";
 import {PageHeaderComponent} from "../../component/page-header/page-header.component";
+import {JourneyService} from "../../service/journey/journey.service";
+import {AsyncPipe, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-dashboard',
@@ -12,42 +12,47 @@ import {PageHeaderComponent} from "../../component/page-header/page-header.compo
   styleUrls: ['./dashboard.component.scss'],
   imports: [
     PageHeaderComponent,
-    WorldMapComponent
+    WorldMapComponent,
+    NgIf,
+    AsyncPipe
   ],
   standalone: true
 })
 export class DashboardComponent implements OnInit {
-  private map!: L.Map;
-  @ViewChild(WorldMapComponent) worldMapComponent!: WorldMapComponent;
   protected readonly DASHBOARD_PAGE_INFO = DASHBOARD_PAGE_INFO;
+  // Tip: Async pipe can not be used due to popup maker component view initialisation hook
   featureCollection: FeatureCollection | undefined;
+  stats: Map<String, number> = new Map<String, number>()
 
-  constructor(private locationService: LocationService) {
+  constructor(
+    private journeyService: JourneyService) {
   }
 
   ngOnInit(): void {
 
-    // Fetch all available location and store it
-    this.locationService.getAllAvailableLocations()
-      .subscribe((featureCollections: FeatureCollection) => {
-        this.featureCollection = featureCollections;
-      });
-
+    this.journeyService.getAllJourneysAsGeoJson()
+      .subscribe(data => this.onFetchSuccessCallback(data));
   }
 
-  onMapInitializationComplete(map: L.Map) {
-    this.map = map;
-    this.centerMapBasedOnUserCurrentLocation();
-
+  onFetchSuccessCallback(featureCollection: FeatureCollection) {
+    this.featureCollection = featureCollection;
+    this.collectStats(featureCollection);
   }
 
-  private centerMapBasedOnUserCurrentLocation() {
-    // Try to get user's current location and based on the location centre div to provide better visualisation
-    navigator.geolocation.getCurrentPosition(
-      (location) => this.map?.setView([location.coords.latitude, location.coords.longitude], 5),
-      (err) => console.warn(err)
-    );
+  collectStats(featureCollection: FeatureCollection) {
+    this.stats.set("Journeys", featureCollection.features.length);
+    this.stats.set("Places", this.uniqueJourneysByProperty(featureCollection, 'location'));
+    this.stats.set("Country", this.uniqueJourneysByProperty(featureCollection, 'country'));
+    this.stats.set("City", this.uniqueJourneysByProperty(featureCollection, 'city'));
   }
 
+  private uniqueJourneysByProperty(featureCollection: FeatureCollection, property: string) {
+    return featureCollection.features
+      .filter(feature => feature.properties?.[property])
+      .filter((feature, i, arr) =>
+        arr.findIndex(f =>
+          JSON.stringify(f.properties?.[property]) === JSON.stringify(feature.properties?.[property])) === i
+      ).length;
+  }
 
 }
