cc006009c978bf192762d876e2edf43386585653
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-04-15 22:59:55 +0200

    [feat] login form ui design added

--- src/app/page/auth/login/login.component.ts
+++ src/app/page/auth/login/login.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule, NgForm} from "@angular/forms";
+import {FeedbackMessageComponent} from "../../../component/feedback-message/feedback-message.component";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    FeedbackMessageComponent,
+    NgIf
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  successMessage: string = '';
+  errorMessage: string = '';
+
+  form: LoginForm = new LoginForm();
+
+  login(loginForm: NgForm) {
+
+  }
 
 }
+
+class LoginForm {
+  constructor(public userName: string = '', public password: string = '') {
+  }
+}
+
