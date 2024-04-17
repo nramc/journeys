a761943d2e578abdca34da121a1e20efb3a47abc
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-04-17 21:50:17 +0200

    [feat] auth utils added to manage local storage for storing user context

--- src/app/service/auth/auth.service.ts
+++ src/app/service/auth/auth.service.ts
@@ -2,27 +2,20 @@ import {Injectable} from '@angular/core';
 import {UserContext} from "./user-context";
 import {BehaviorSubject, map} from "rxjs";
 import {LoginResponse, LoginService} from "./login.service";
+import AuthUtils from "./auth.utils";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private USER_CONTEXT_KEY: string = '_auth';
   private user$: BehaviorSubject<UserContext> = new BehaviorSubject<UserContext>(new UserContext());
 
   constructor(
     private loginService: LoginService
   ) {
-    let localAuth = localStorage.getItem(this.USER_CONTEXT_KEY);
+    let localAuth = AuthUtils.getUserContextFromLocalStorage();
     if (localAuth) {
-      let localUserContext = JSON.parse(localAuth) as UserContext;
-      if (new Date() < localUserContext.expiredAt) {
-        this.user$.next(localUserContext);
-        console.log('Local auth context found and valid.', localAuth);
-      } else {
-        localStorage.removeItem(this.USER_CONTEXT_KEY);
-        console.log('Local auth context found, but not valid. Thus removed from local storage.', localAuth);
-      }
+      this.user$.next(localAuth);
     }
   }
 
@@ -50,7 +43,7 @@ export class AuthService {
       tokenData.authorities,
       tokenData.token,
       tokenData.expiredAt);
-    localStorage.setItem(this.USER_CONTEXT_KEY, JSON.stringify(userContext));
+    AuthUtils.saveUserContextInLocalStorage(userContext)
     this.user$.next(userContext);
     return userContext;
   }
