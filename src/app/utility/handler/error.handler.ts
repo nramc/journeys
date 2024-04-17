944a100f0718bddfe30c2add94f4485cd703a4dd
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-04-17 15:47:32 +0200

    [feat] custom error handler added

---
+++ src/app/utility/handler/error.handler.ts
@@ -0,0 +1,36 @@
+import {ErrorHandler, Injectable} from "@angular/core";
+import {HttpErrorResponse} from "@angular/common/http";
+import {throwError} from "rxjs";
+import {Router} from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomErrorHandler implements ErrorHandler {
+  constructor(private router: Router) {
+  }
+
+  handleError(err: any) {
+    if (err && err instanceof HttpErrorResponse) {
+      this.handleHttpError(err);
+    }
+    return throwError(() => err)
+
+  }
+
+
+  private handleHttpError(err: HttpErrorResponse) {
+    switch (err.status) {
+      case 401:
+        this.router.navigate(['/login'], {queryParams: {'q': 'unauthenticated'}})
+          .then(() => console.log('Authentication required, redirected to login page'));
+        break;
+      case 403:
+        this.router.navigate(['/accessDenied'], {queryParams: {'q': 'unauthorized'}})
+          .then(() => console.log('Authentication required, redirected to login page'));
+        break;
+      default:
+        console.log('no http handler defined');
+    }
+  }
+}
