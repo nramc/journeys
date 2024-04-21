08a01d6a05b25defae4c9662b37a4619e477769c
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-04-21 23:14:06 +0200

    [feat] new component added to display journey basic details #113

--- src/app/page/journeys/view-journey/view-journey-basic-details/view-journey-basic-details.component.ts
+++ src/app/page/journeys/view-journey/view-journey-basic-details/view-journey-basic-details.component.ts
@@ -1,10 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {Journey} from "../../../../model/core/journey.model";
+import {DatePipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-view-journey-basic-details',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe,
+    NgForOf
+  ],
   templateUrl: './view-journey-basic-details.component.html',
   styleUrl: './view-journey-basic-details.component.scss'
 })
