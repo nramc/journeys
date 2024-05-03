e954195c6ce9b2f8ebe03a56a910d6e6d25fd6cf
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-05-03 15:16:57 +0200

    feat: subject subscriptions unsubscribed #78

--- src/app/directive/has-role.directive.ts
+++ src/app/directive/has-role.directive.ts
@@ -1,8 +1,8 @@
-import {Directive, inject, Input, OnInit} from '@angular/core';
+import {Directive, inject, Input, OnDestroy, OnInit} from '@angular/core';
 import {NgIf} from "@angular/common";
 import {DestroyedDirective} from "./destroyed-directive.directive";
 import {AuthService} from "../service/auth/auth.service";
-import {takeUntil} from "rxjs";
+import {Subscription, takeUntil} from "rxjs";
 import {Role} from "../service/auth/role";
 
 @Directive({
@@ -10,7 +10,8 @@ import {Role} from "../service/auth/role";
   standalone: true,
   hostDirectives: [NgIf, DestroyedDirective]
 })
-export class HasRoleDirective implements OnInit {
+export class HasRoleDirective implements OnInit, OnDestroy {
+  private subscriptions = new Subscription();
   private readonly ngIfDirective = inject(NgIf);
   private readonly destroyed$ = inject(DestroyedDirective).destroyed$;
   expectedRoles: Role[] = [];
@@ -24,11 +25,16 @@ export class HasRoleDirective implements OnInit {
   }
 
   ngOnInit(): void {
-    this.authService.getUserContext()
+    this.subscriptions.add(this.authService.getUserContext()
       .pipe(takeUntil(this.destroyed$))
       .subscribe((userContext) => {
         this.ngIfDirective.ngIf = userContext.roles.some(role => this.expectedRoles.indexOf(role) != -1);
-      });
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
 
