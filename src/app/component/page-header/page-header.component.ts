3fea234680c8a9c7a38992aa83265839a155b4fc
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2024-05-08 19:03:21 +0200

    167 suggestion gallery card handle on click event to redirect to view journey page (#168)
    * fix: multiple cosmetic alignment fix
    
    * fix: new love icon added
    
    ---------
    
    Co-authored-by: Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>

--- src/app/component/page-header/page-header.component.ts
+++ src/app/component/page-header/page-header.component.ts
@@ -1,9 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {PageInfo} from "../../model/page-info";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-page-header',
   templateUrl: './page-header.component.html',
+  imports: [
+    NgIf
+  ],
   standalone: true
 })
 export class PageHeaderComponent {
