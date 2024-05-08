1fc0e7f7488f0082f4bbea6b9606e7ef7e45526a
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-05-08 18:35:50 +0200

    fix: multiple cosmetic alignment fix

--- src/app/model/page-info.ts
+++ src/app/model/page-info.ts
@@ -26,32 +26,32 @@ export const LOGOUT_PAGE_INFO: PageInfo = {
 export const JOURNEY_SEARCH_PAGE_INFO: PageInfo = {
   name: "Journeys",
   title: "Journeys",
-  description: "\"Life is a treasure hunt, always searching for those special moments.\" - Anonymous.",
+  description: "",
   path: "/journey"
 };
 export const NEW_JOURNEY_PAGE_INFO: PageInfo = {
   name: "GeoPin",
   title: "New Journey",
-  description: "\"Memories are the only real treasure. Preserve them.\" - Anonymous.",
+  description: "",
   path: "/journey/new"
 };
 export const VIEW_JOURNEY_PAGE_INFO: PageInfo = {
   name: "ViewJourney",
   title: "View Journey",
-  description: "\"We are all travelers in the desert of this world. We are all searching for some oasis of meaning.\" - E.F. Schumacher.",
+  description: "",
   path: "/journey/{id}/view"
 };
 
 export const DASHBOARD_PAGE_INFO: PageInfo = {
   name: "Dashboard",
   title: "Dashboard",
-  description: "The goal is to turn data into information and information into insight.",
+  description: "",
   path: "/dashboard"
 };
 export const GALLERY_PAGE_INFO: PageInfo = {
   name: "Gallery",
   title: "Gallery",
-  description: "Life is a Collection of Memories. Memories are the Greatest Treasure. Find your Treasure.",
+  description: "",
   path: "/gallery"
 };
 
