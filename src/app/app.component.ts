270db5aefc43b379fe0e480f5177a0c6621464a0
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2024-05-09 16:57:33 +0200

    fix: base font size reduced for windows os for layout shift issues (#174)
    Co-authored-by: Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Journey';
+
+  ngOnInit(): void {
+    // Check for Windows OS
+    const isWindows = navigator.userAgent.toLowerCase().indexOf('win') > -1;
+    if (isWindows) {
+      document.getElementsByTagName('html')?.item(0)?.classList.add('base-fs-windows');
+    }
+  }
 }
