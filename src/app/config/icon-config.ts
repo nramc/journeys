4db761b4caff1e58dfd6c7f498084308fc6b7059
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-05-12 20:31:23 +0200

    feat: shopping icon added

--- src/app/config/icon-config.ts
+++ src/app/config/icon-config.ts
@@ -1,7 +1,7 @@
 import * as L from "leaflet";
 import {Feature} from "geojson";
 
-export const SUPPORTED_ICONS: string[] = ['default', 'adventure', 'park', 'restaurant', 'home', 'flight', 'temple', 'favorite', 'love'];
+export const SUPPORTED_ICONS: string[] = ['default', 'adventure', 'shopping', 'park', 'restaurant', 'home', 'flight', 'temple', 'favorite', 'love'];
 
 export const iconHome = L.divIcon({
   className: 'custom-div-icon',
@@ -78,6 +78,16 @@ export const iconLove = L.divIcon({
   shadowSize: [41, 41]
 });
 
+export const iconShopping = L.divIcon({
+  className: 'custom-div-icon',
+  html: "<div class='marker-icon-wrapper'><div class='marker-pin'></div><i class='material-icons text-success fs-5'>local_mall</i></div>",
+  iconSize: [30, 42],
+  iconAnchor: [15, 42],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+
 export const iconDefault = L.divIcon({
   className: 'custom-div-icon',
   html: "<div class='marker-icon-wrapper'><div class='marker-pin'></div><i class='material-icons text-primary fs-5'>navigation</i></div>",
@@ -122,6 +132,8 @@ function getIconForType(iconType: string) {
         return iconAdventure;
       case "park":
         return iconPark;
+      case "shopping":
+        return iconShopping;
       default:
         return iconDefault;
     }
