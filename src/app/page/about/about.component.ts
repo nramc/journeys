c23e2d88a6e47ade86be412a6835fd8d540f6719
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-05-18 22:30:37 +0200

    feat: about page updated with contributors

--- src/app/page/about/about.component.ts
+++ src/app/page/about/about.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {PageHeaderComponent} from "../../component/page-header/page-header.component";
+import {ABOUT_PAGE_INFO} from "../../model/page.info.model";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    PageHeaderComponent,
+    NgOptimizedImage
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
 
+  protected readonly ABOUT_PAGE_INFO = ABOUT_PAGE_INFO;
 }
