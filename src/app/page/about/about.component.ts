d671d1960c8a02deace241d5f3c80cc3929f9af9
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-05-19 12:22:38 +0200

    feat: about page updated with introduction

--- src/app/page/about/about.component.ts
+++ src/app/page/about/about.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {PageHeaderComponent} from "../../component/page-header/page-header.component";
 import {ABOUT_PAGE_INFO} from "../../model/page.info.model";
 import {NgOptimizedImage} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-about',
@@ -14,6 +15,12 @@ import {NgOptimizedImage} from "@angular/common";
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
   protected readonly ABOUT_PAGE_INFO = ABOUT_PAGE_INFO;
+
+  constructor(private router: Router) {
+  }
+
+  signup() {
+    this.router.navigate(['/login']).then(console.log);
+  }
 }
