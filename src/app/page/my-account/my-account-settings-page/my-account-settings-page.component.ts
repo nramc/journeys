1604524af27535e7a03aa11724ce4e8d430b069b
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-07-06 21:39:39 +0200

    feat: security page added
     - prototype added

--- src/app/page/my-account/my-account-settings-page/my-account-settings-page.component.ts
+++ src/app/page/my-account/my-account-settings-page/my-account-settings-page.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MY_SECURITY_SETTINGS_PAGE_INFO} from "../../../model/page.info.model";
+import {PageHeaderComponent} from "../../../component/page-header/page-header.component";
+import {MyEmailAddressSettingsComponent} from "./my-email-address-settings/my-email-address-settings.component";
 
 @Component({
   selector: 'app-my-account-settings-page',
   standalone: true,
-  imports: [],
+  imports: [
+    PageHeaderComponent,
+    MyEmailAddressSettingsComponent
+  ],
   templateUrl: './my-account-settings-page.component.html',
   styleUrl: './my-account-settings-page.component.scss'
 })
 export class MyAccountSettingsPageComponent {
+  protected readonly MY_SECURITY_SETTINGS_PAGE_INFO = MY_SECURITY_SETTINGS_PAGE_INFO;
 
 }
