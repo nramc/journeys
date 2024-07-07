c93e0561c482a65d64bcf2f470849e85851dd53e
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-07-07 13:18:25 +0200

    feat: email code verification component added

---
+++ src/app/component/security/email-code-verification/email-code-verification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-code-verification',
+  standalone: true,
+  imports: [],
+  templateUrl: './email-code-verification.component.html',
+  styleUrl: './email-code-verification.component.scss'
+})
+export class EmailCodeVerificationComponent {
+
+}
