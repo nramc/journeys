e13334ea80f686be4622d0dc33ad8f79ba094369
Ram <ramachandrannellai@gmail.com>
2024-07-13 20:05:06 +0200

    feat: totp multi-factor authentication prototype added

---
+++ src/app/page/my-account/my-account-settings-page/my-totp-settings/my-totp-settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-totp-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-totp-settings.component.html',
+  styleUrl: './my-totp-settings.component.scss'
+})
+export class MyTotpSettingsComponent {
+
+}
