36c4b126eb0141f4f9d5f2ef1a63a4fed3e20cdd
Ram <ramachandrannellai@gmail.com>
2024-07-28 19:40:34 +0200

    feat: HTTP Observables to signal base implementation added

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,15 +1,13 @@
-import {Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {HOME_PAGE_INFO} from "../../model/page.info.model";
 import {PageHeaderComponent} from "../../component/page-header/page-header.component";
 import {AuthService} from "../../service/auth/auth.service";
 import {AsyncPipe, JsonPipe, NgIf} from "@angular/common";
 import {environment} from "../../../environments/environment";
 import {BffService} from "../../service/bff/bff.service";
-import {ActivatedRoute, RouterLink} from "@angular/router";
-import {exhaustMap, mergeMap} from "rxjs";
+import {RouterLink} from "@angular/router";
 import {TimelineService} from "../../service/timeline/timeline.service";
-import {TimelineData} from "../../component/timeline/timeline-data.model";
-import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {toSignal} from "@angular/core/rxjs-interop";
 import {TimelineComponent} from "../../component/timeline/timeline.component";
 
 @Component({
@@ -26,45 +24,17 @@ import {TimelineComponent} from "../../component/timeline/timeline.component";
   ],
   standalone: true
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   protected readonly HOME_PAGE_INFO = HOME_PAGE_INFO;
+
+  protected authService = inject(AuthService);
+  private bffService = inject(BffService);
+  private timelineService = inject(TimelineService);
+
   appVersion = environment.version;
-  bffApiVersion: string | 'CHECKING' | 'NOT_AVAILABLE' = 'CHECKING';
-  timelineData: TimelineData | undefined;
-
-  private destroyRef = inject(DestroyRef);
-
-
-  constructor(
-    protected authService: AuthService,
-    private bffService: BffService,
-    private activatedRoute: ActivatedRoute,
-    private timelineService: TimelineService
-  ) {
-  }
-
-  ngOnInit(): void {
-    // BFF Service availability check
-    this.activatedRoute.params
-      .pipe(exhaustMap(__ => this.bffService.getVersion()))
-      .subscribe({
-        next: result => this.bffApiVersion = 'v' + result.version,
-        error: err => {
-          console.error(err);
-          this.bffApiVersion = 'NOT_AVAILABLE';
-        }
-      });
-
-    // Today in history timeline data
-    this.authService.getUserContext()
-      .pipe(
-        takeUntilDestroyed(this.destroyRef),
-        mergeMap(_ => this.timelineService.getTimelineForToday())
-      ).subscribe({
-      next: data => this.timelineData = data,
-      error: err => console.error(err)
-    });
-  }
+  userData = toSignal(this.authService.getUserContext());
+  bffApiVersion = toSignal(this.bffService.getVersion());
+  timelineData = toSignal(this.timelineService.getTimelineForToday());
 
 }
 
