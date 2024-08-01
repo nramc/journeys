9b933bba2c4e676c100e92beb16be2bb94a44d40
Ram <ramachandrannellai@gmail.com>
2024-08-01 22:59:19 +0200

    feat: signup base template added

--- src/app/page/auth/signup/signup.component.ts
+++ src/app/page/auth/signup/signup.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FeedbackMessageComponent} from "../../../component/feedback-message/feedback-message.component";
+import {FormsModule, NgForm, ReactiveFormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {LOGIN_PAGE_INFO} from "../../../model/page.info.model";
+
+export class SignupForm {
+  constructor(
+    public name: string = '',
+    public username: string = '',
+    public password: string = ''
+  ) {
+  }
+}
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [
+    FeedbackMessageComponent,
+    FormsModule,
+    NgIf,
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  protected readonly LOGIN_PAGE_INFO = LOGIN_PAGE_INFO;
 
+  form = new SignupForm();
+
+  signup(signupForm: NgForm) {
+    if (signupForm.valid) {
+      console.log('data valid');
+    } else {
+      console.log('data not valid');
+    }
+  }
 }
