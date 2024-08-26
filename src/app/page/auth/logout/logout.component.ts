12e3feaa52bdfec93d433267dc92649207e8d29f
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2024-08-26 22:20:14 +0200

    feat: material logout dialog replaced with native html dialog component (#363)

--- src/app/page/auth/logout/logout.component.ts
+++ src/app/page/auth/logout/logout.component.ts
@@ -1,35 +1,22 @@
-import {Component, DestroyRef, inject, OnInit, TemplateRef, viewChild} from '@angular/core';
+import {Component, ElementRef, inject, OnInit, viewChild} from '@angular/core';
 import {AuthService} from "../../../service/auth/auth.service";
 import {Router} from "@angular/router";
-import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
-import {MatDialog, MatDialogModule} from "@angular/material/dialog";
 import {MatIcon} from "@angular/material/icon";
-import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-logout',
   standalone: true,
-  imports: [MatDialogModule, MatIcon, MatButton],
+  imports: [MatIcon],
   templateUrl: './logout.component.html',
   styleUrl: './logout.component.scss'
 })
 export class LogoutComponent implements OnInit {
-  private destroyRef = inject(DestroyRef);
-  readonly dialog = inject(MatDialog);
   private authService: AuthService = inject(AuthService);
   private router: Router = inject(Router);
-  logoutDialogContent = viewChild.required<TemplateRef<any>>('logoutDialogContent');
+  logoutDialog = viewChild.required<ElementRef<HTMLDialogElement>>('logoutDialog');
 
   ngOnInit(): void {
-    const dialogRef = this.dialog.open(this.logoutDialogContent(), {disableClose: true});
-    dialogRef.afterClosed().pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(result => {
-        if (result === true) {
-          this.logout();
-        } else {
-          this.home()
-        }
-      });
+    this.logoutDialog().nativeElement.showModal();
   }
 
   logout() {
