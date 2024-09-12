a5526ebe5250ca26411db6001b56ed7784d169c9
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2024-09-12 21:50:21 +0200

    feat: confirmation dialog added to delete journey action (#402)
    * feat: feedback-message.component.ts updated to add loading message
    
    * feat: confirmation dialog added to delete journey action

--- src/app/component/feedback-message/feedback-message.component.ts
+++ src/app/component/feedback-message/feedback-message.component.ts
@@ -1,10 +1,13 @@
 import {Component, input} from '@angular/core';
 import {FeedbackMessage} from "./feedback-message";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-feedback-message',
   standalone: true,
-  imports: [],
+  imports: [
+    MatProgressSpinner
+  ],
   templateUrl: './feedback-message.component.html',
   styleUrl: './feedback-message.component.scss'
 })
