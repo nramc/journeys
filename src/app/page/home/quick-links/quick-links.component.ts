067ca1cfca17bae36687f3612234ac23e0f76777
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2024-09-14 21:10:38 +0200

    feat: quick links added as dedicated components (#417)

---
+++ src/app/page/home/quick-links/quick-links.component.ts
@@ -0,0 +1,23 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {NEW_JOURNEY_PAGE_INFO, TIMELINE_PAGE_INFO} from "../../../model/page.info.model";
+
+@Component({
+  selector: 'app-quick-links',
+  standalone: true,
+  imports: [
+    MatIcon,
+    NgIf,
+    RouterLink
+  ],
+  templateUrl: './quick-links.component.html',
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class QuickLinksComponent {
+
+  protected readonly NEW_JOURNEY_PAGE_INFO = NEW_JOURNEY_PAGE_INFO;
+  protected readonly TIMELINE_PAGE_INFO = TIMELINE_PAGE_INFO;
+}
