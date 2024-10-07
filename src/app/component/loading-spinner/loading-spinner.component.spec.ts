74948c192d1e20cb2b103547da55ab24cb7879cb
Ram <ramachandrannellai@gmail.com>
2024-10-07 22:45:58 +0200

    feat:  component refactored with signal api and loading spinner

--- src/app/component/loading-spinner/loading-spinner.component.spec.ts
+++ src/app/component/loading-spinner/loading-spinner.component.spec.ts
@@ -3,14 +3,24 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {LoadingSpinnerComponent} from './loading-spinner.component';
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {By} from "@angular/platform-browser";
+import {BehaviorSubject} from "rxjs";
+import {LoadingService} from "../../service/common/loading.service";
 
 describe('LoadingSpinnerComponent', () => {
   let component: LoadingSpinnerComponent;
   let fixture: ComponentFixture<LoadingSpinnerComponent>;
 
+  let loading$ = new BehaviorSubject<boolean>(false);
+  const loadingServiceMock = {
+    loading$: loading$.asObservable() // Simulate loading observable behavior
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoadingSpinnerComponent, MatProgressSpinnerModule]
+      imports: [LoadingSpinnerComponent, MatProgressSpinnerModule],
+      providers: [
+        {provide: LoadingService, useValue: loadingServiceMock}
+      ]
     })
       .compileComponents();
 
@@ -23,12 +33,12 @@ describe('LoadingSpinnerComponent', () => {
     expect(component).toBeTruthy();
   });
   it('should display spinner when loading = true', () => {
-    component.isLoading.set(true);
+    loading$.next(true);
     fixture.detectChanges();
     expect(fixture.debugElement.query(By.css('mat-spinner'))).toBeTruthy();
   });
   it('should hide spinner when loading = false', () => {
-    component.isLoading.set(false);
+    loading$.next(false);
     fixture.detectChanges();
     expect(fixture.debugElement.query(By.css('mat-spinner'))).toBeFalsy();
   });
