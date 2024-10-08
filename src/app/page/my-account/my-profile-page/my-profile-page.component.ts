96ee1dbe706932d10a19659551bc5f49cf55e116
Ram <ramachandrannellai@gmail.com>
2024-10-08 20:23:26 +0200

    feat: MyProfilePageComponent refactored with signal and legacy feedback implementation removed

--- src/app/page/my-account/my-profile-page/my-profile-page.component.ts
+++ src/app/page/my-account/my-profile-page/my-profile-page.component.ts
@@ -1,18 +1,17 @@
-import {Component, computed, DestroyRef, inject, OnInit, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
 import {MyAccountService} from "../../../service/my-account/my-account.service";
-import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
-import {AppUser} from "../../../model/account/app-user";
+import {toSignal} from "@angular/core/rxjs-interop";
 import {NgForOf, NgIf} from "@angular/common";
 import {MY_PROFILE_PAGE_INFO} from "../../../model/page.info.model";
 import {PageHeaderComponent} from "../../../component/page-header/page-header.component";
 import {FormsModule, NgForm} from "@angular/forms";
 import {Router} from "@angular/router";
 import {FeedbackMessageComponent} from "../../../component/feedback-message/feedback-message.component";
-import {FeedbackMessage} from "../../../component/feedback-message/feedback-message";
 import {DisableIfNoRoleExistsDirective} from "../../../directive/disable-if-no-role-exists.directive";
 import {AuthService} from "../../../service/auth/auth.service";
 import {Role} from "../../../service/auth/role";
 import {HasWriteAccessDirective} from "../../../directive/has-write-access.directive";
+import {NotificationService} from "../../../service/common/notification.service";
 
 @Component({
   selector: 'app-my-profile-page',
@@ -27,51 +26,39 @@ import {HasWriteAccessDirective} from "../../../directive/has-write-access.direc
     HasWriteAccessDirective
   ],
   templateUrl: './my-profile-page.component.html',
-  styleUrl: './my-profile-page.component.scss'
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MyProfilePageComponent implements OnInit {
+export class MyProfilePageComponent {
   protected readonly MY_PROFILE_PAGE_INFO = MY_PROFILE_PAGE_INFO;
-  private destroyRef = inject(DestroyRef);
+  protected readonly Role = Role;
 
-  feedbackMessage = signal<FeedbackMessage>({});
+  private readonly notificationService = inject(NotificationService);
+  private readonly myAccountService = inject(MyAccountService);
+  private readonly router = inject(Router);
+  protected readonly authService = inject(AuthService);
 
-  mydata: AppUser | undefined;
+  myProfileData = toSignal(this.myAccountService.getProfileData());
   hasAccess = computed(() => this.authService.hasAnyRole([Role.AUTHENTICATED_USER]));
-
-  constructor(
-    private myAccountService: MyAccountService,
-    private router: Router,
-    protected authService: AuthService) {
-  }
-
-  ngOnInit(): void {
-    this.myAccountService.getProfileData()
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe({
-        next: data => this.mydata = data,
-        error: err => console.error(err)
-      })
-  }
 
   save(profileForm: NgForm) {
     if (profileForm.valid && profileForm.dirty) {
-      this.myAccountService.saveProfileData(this.mydata!).subscribe(
+      this.myAccountService.saveProfileData(this.myProfileData()!).subscribe(
         {next: _ => this.onSuccessCallback(), error: _ => this.onErrorCallback()}
       );
     }
   }
 
   onSuccessCallback() {
-    this.feedbackMessage.set({success: 'Profile data saved successfully'});
+    this.notificationService.showSuccess('Profile data saved successfully');
   }
 
   onErrorCallback() {
-    this.feedbackMessage.set({error: 'Update failed. Please check data.'});
+    this.notificationService.showError('Update failed. Please check data.');
   }
 
   cancel() {
     this.router.navigate(['/']).then(console.log);
   }
 
-  protected readonly Role = Role;
 }
