d714a8aa5305899a916c7fbb905ecf8b540d6a9f
Ram <ramachandrannellai@gmail.com>
2024-10-13 21:38:59 +0200

    feat: dark theme style added and unwanted Windows os workaround style removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SideNavbarComponent } from './component/side-navbar/side-navbar.component';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {SideNavbarComponent} from './component/side-navbar/side-navbar.component';
 import {LoadingSpinnerComponent} from "./component/loading-spinner/loading-spinner.component";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
   imports: [SideNavbarComponent, RouterOutlet, LoadingSpinnerComponent]
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Journey';
-
-  ngOnInit(): void {
-    // Check for Windows OS
-    const isWindows = navigator.userAgent.toLowerCase().indexOf('win') > -1;
-    if (isWindows) {
-      document.getElementsByTagName('html')?.item(0)?.classList.add('base-fs-windows');
-    }
-  }
 }
