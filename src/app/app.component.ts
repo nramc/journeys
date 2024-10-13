7f412673b265ff2700b36f6cc14a8cf050fb203a
Ram <ramachandrannellai@gmail.com>
2024-10-13 22:14:22 +0200

    feat: theme toggle added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {SideNavbarComponent} from './component/side-navbar/side-navbar.component';
 import {LoadingSpinnerComponent} from "./component/loading-spinner/loading-spinner.component";
+import {ThemeToggleComponent} from "./component/theme-toggle/theme-toggle.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [SideNavbarComponent, RouterOutlet, LoadingSpinnerComponent]
+  imports: [SideNavbarComponent, RouterOutlet, LoadingSpinnerComponent, ThemeToggleComponent]
 })
 export class AppComponent {
   title = 'Journey';
---
+++ src/app/component/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,42 @@
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {MatIconModule} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+
+@Component({
+  selector: 'app-theme-toggle',
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatIconButton
+  ],
+  template: `
+    <button mat-icon-button (click)="toggleTheme()"
+            [title]="isDarkMode() ? 'Switch to light mode' : 'Switch to dark mode'">
+      <mat-icon class="text-primary">
+        {{ isDarkMode() ? 'dark_mode' : 'light_mode' }}
+      </mat-icon>
+    </button>
+  `,
+  styles: `
+    button {
+      top: 0;
+      right: 0;
+      float: right;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ThemeToggleComponent {
+  isDarkMode = signal<boolean>(false);
+
+  constructor() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    document.body.classList.toggle('dark-theme', prefersDark.matches);
+    this.isDarkMode.set(prefersDark.matches);
+  }
+
+  toggleTheme() {
+    this.isDarkMode.update(value => !value);
+    document.body.classList.toggle('dark-theme');
+  }
+}
