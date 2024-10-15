2065fa76c2b8231add5770c70b4045da6d0de2f1
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2024-10-15 21:46:57 +0200

    feat: resize image tool refactored use signal (#515)

--- src/app/component/image-resize/image-resize.component.ts
+++ src/app/component/image-resize/image-resize.component.ts
@@ -1,9 +1,15 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import JSZip from "jszip";
 import {saveAs} from 'file-saver';
 import {MatProgressBar} from "@angular/material/progress-bar";
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
+
+
+export interface ImageResource {
+  dataUrl: string,
+  name: string
+}
 
 @Component({
   selector: 'app-image-resize',
@@ -22,16 +28,16 @@ import {MatProgressSpinner} from "@angular/material/progress-spinner";
           <input type="file" (change)="onFileChange($event)" multiple placeholder="Choose files resize"
                  class="form-control text-primary border border-primary"/>
         </div>
-        <div class="col text-center" *ngIf="totalImages > 0">
-          <p class="text-primary">Resized Images: {{ resizedImages.length }}
+        <div class="col text-center" *ngIf="totalImages() > 0">
+          <p class="text-primary">Resized Images: {{ resizedImages().length }}
             <span
-              *ngIf="totalImages - resizedImages.length > 0">Remaining: {{ totalImages - resizedImages.length }}</span>
+              *ngIf="totalImages() - resizedImages().length > 0">Remaining: {{ totalImages() - resizedImages().length }}</span>
           </p>
           <mat-progress-bar *ngIf="isProcessing" mode="determinate"
-                            [value]="(resizedImages.length/totalImages)*100"></mat-progress-bar>
+                            [value]="(resizedImages().length/totalImages())*100"></mat-progress-bar>
         </div>
         <div class="col text-center">
-          <button (click)="downloadAll()" *ngIf="resizedImages.length == totalImages && totalImages > 0"
+          <button (click)="downloadAll()" *ngIf="resizedImages().length == totalImages() && totalImages() > 0"
                   class="btn btn-sm btn-outline-primary">
             Download All
           </button>
@@ -39,19 +45,20 @@ import {MatProgressSpinner} from "@angular/material/progress-spinner";
       </div>
     </section>
   `,
-  styles: []
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImageResizeComponent {
-  protected isProcessing = false;
-  totalImages: number = 0;
-  resizedImages: { dataUrl: string, name: string }[] = [];
+  protected isProcessing = signal(false);
+  totalImages = signal<number>(0);
+  resizedImages = signal<ImageResource[]>([]);
 
   onFileChange(event: any) {
-    this.isProcessing = true;
+    this.isProcessing.set(true);
     const files = event.target.files;
-    this.totalImages = files.length;
+    this.totalImages.set(files.length);
     if (files.length > 0) {
-      this.resizedImages = [];
+      this.resizedImages.set([]);
       for (const element of files) {
         const file = element;
         const reader = new FileReader();
@@ -78,20 +85,21 @@ export class ImageResizeComponent {
             canvas.height = height;
             ctx.drawImage(img, 0, 0, width, height);
 
-            this.resizedImages.push({dataUrl: canvas.toDataURL('image/jpeg'), name: file.name});
+            this.resizedImages.update(data =>
+              [...data, {dataUrl: canvas.toDataURL('image/jpeg'), name: file.name}]);
           };
           img.src = e.target.result;
         };
         reader.readAsDataURL(file);
       }
     }
-    this.isProcessing = false;
+    this.isProcessing.set(false);
   }
 
 
   downloadAll() {
     const zip = new JSZip();
-    this.resizedImages.forEach((image) => {
+    this.resizedImages().forEach((image) => {
       const imgData = image.dataUrl.split(',')[1];
       zip.file(image.name, imgData, {base64: true});
     });
