d3191cf35006e57e9d9fb57351472aa3406cc324
Ram <ramachandrannellai@gmail.com>
2024-10-16 19:32:36 +0200

    fix: tests which  required zone.js removed

--- src/app/service/common/notification.service.ts
+++ src/app/service/common/notification.service.ts
@@ -8,17 +8,19 @@ export class NotificationService {
   private readonly snackBar = inject(MatSnackBar);
 
   showSuccess(message: string, duration: number = 3000) {
-    this.snackBar.open(message, 'Close', {
+    // wrapped notification into setTimeout to trigger change detection manually since material does not support zoneless change detection yet.
+    setTimeout(() => this.snackBar.open(message, 'Close', {
       duration: duration,
       panelClass: ['success-snackbar'] // Add a custom CSS class for success
-    });
+    }));
   }
 
   showError(message: string, duration: number = 3000) {
-    this.snackBar.open(message, 'Close', {
+    // wrapped notification into setTimeout to trigger change detection manually since material does not support zoneless change detection yet.
+    setTimeout(() => this.snackBar.open(message, 'Close', {
       duration: duration,
       panelClass: ['error-snackbar'] // Add a custom CSS class for error
-    });
+    }));
   }
 }
 
