9c15aa593d8cb040daeb3ba04123982098e9315f
Ram <ramachandrannellai@gmail.com>
2024-10-17 20:37:58 +0200

    feat: narration component added to handle markdown

---
+++ src/app/component/narration/narration.component.ts
@@ -0,0 +1,26 @@
+import {Component, input, model, signal} from '@angular/core';
+import {DisplayMarkdownComponent} from "../display-markdown-component/display-markdown.component";
+import {FormsModule} from "@angular/forms";
+import {MatButtonToggle, MatButtonToggleGroup} from "@angular/material/button-toggle";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-narration',
+  standalone: true,
+  imports: [
+    DisplayMarkdownComponent,
+    FormsModule,
+    MatButtonToggle,
+    MatButtonToggleGroup,
+    NgIf
+  ],
+  templateUrl: './narration.component.html',
+  styleUrl: './narration.component.scss'
+})
+export class NarrationComponent {
+  markdownStyle = signal<string>('Source')
+
+  title = input<string>('');
+  narration = model<string>('');
+
+}
