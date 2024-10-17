5eb0904bb2f773c620c6f9e5a6af480c7bd5ae42
Ram <ramachandrannellai@gmail.com>
2024-10-17 19:23:15 +0200

    feat: create journey component - stepper added with prototype

--- src/app/page/journeys/create-journey/create-journey.component.ts
+++ src/app/page/journeys/create-journey/create-journey.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+import {MatStepperModule} from "@angular/material/stepper";
+import {NgIf} from "@angular/common";
+import {Journey} from "../../../model/core/journey.model";
 
 @Component({
   selector: 'app-create-journey',
   standalone: true,
-  imports: [],
+  imports: [
+    MatStepperModule,
+    NgIf
+  ],
   templateUrl: './create-journey.component.html',
   styleUrl: './create-journey.component.scss'
 })
 export class CreateJourneyComponent {
+  journey = signal<Journey>(new Journey());
+
+  isBasicDetailsAvailable = computed<boolean>(() =>
+    this.journey().id !== undefined && this.journey().id !== '' && this.journey().id !== null)
 
 }
