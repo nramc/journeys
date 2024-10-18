749f516bc3b8deab4f20b6ab0ba8e3882ddd1861
Ram <ramachandrannellai@gmail.com>
2024-10-18 17:16:58 +0200

    feat: new geolocation component added to manage geolocation better

--- src/app/page/journeys/commons/edit-geo-json/edit-geo-json.component.ts
+++ src/app/page/journeys/commons/edit-geo-json/edit-geo-json.component.ts
@@ -1,12 +1,14 @@
-import {Component, computed, model} from '@angular/core';
+import {Component, computed, model, signal} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {GeoJSON} from "geojson";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-edit-geo-json',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    MatIcon
   ],
   templateUrl: './edit-geo-json.component.html',
   styleUrl: './edit-geo-json.component.scss'
@@ -14,7 +16,7 @@ import {GeoJSON} from "geojson";
 export class EditGeoJsonComponent {
   geoJson = model<GeoJSON | undefined>(undefined)
   geoJsonString = computed(() => JSON.stringify(this.geoJson(), null, 2));
-  //todo is GeoJsonValid
+  isGeoJsonValid = signal<boolean>(true);
 
   onGeoJsonChange(event: Event) {
     const target = event.target as HTMLTextAreaElement;
@@ -23,8 +25,10 @@ export class EditGeoJsonComponent {
       const parsedGeoJson = JSON.parse(jsonStringValue);
       if (parsedGeoJson.type === 'FeatureCollection') {
         this.geoJson.set(parsedGeoJson);
+        this.isGeoJsonValid.set(true);
       }
     } catch (error) {
+      this.isGeoJsonValid.set(false);
       console.error('Invalid GeoJSON:', error);
     }
 
