9b741012e5624f6b78a0c23ff0d35e822067031c
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2024-10-28 22:00:26 +0100

    562-feature-improve-journey-card-view-with-timeline-button (#565)
    * feat: journey card view updated with minor style changes
    
    * feat: journey card view updated with dedicated model
    
    * feat: journey card view updated with dedicated model
    
    * feat: journey card view integrated into dashboard page
    
    * feat: hasOwnProperty replaced with Object.hasOwn

--- src/app/component/marker-popup/marker-popup.component.ts
+++ src/app/component/marker-popup/marker-popup.component.ts
@@ -1,15 +1,23 @@
-import {ChangeDetectionStrategy, Component, ElementRef, input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, ElementRef, input} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {NgForOf} from "@angular/common";
 import {Feature} from "geojson";
+import {JourneyData} from "../journey-card-view/journey.data";
+import {JourneyCardViewComponent} from "../journey-card-view/journey-card-view.component";
+import {DEFAULT_CATEGORY, DEFAULT_THUMBNAIL} from "../../model/core/journey.model";
 
 @Component({
   selector: 'app-marker-popup',
   templateUrl: './marker-popup.component.html',
-  styles: [],
+  styles: `
+  ::ng-deep .leaflet-popup-content {
+    min-width: 15rem !important;
+  }
+  `,
   imports: [
     RouterLink,
-    NgForOf
+    NgForOf,
+    JourneyCardViewComponent
   ],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -17,6 +25,15 @@ import {Feature} from "geojson";
 export class MarkerPopupComponent {
 
   public feature = input<Feature>();
+  journeyData = computed<JourneyData>(() => new JourneyData(
+    '' + this.feature()?.id,
+    this.feature()?.properties?.['name'] ?? '',
+    this.feature()?.properties?.['title'] ?? '',
+    this.feature()?.properties?.['category'] ?? DEFAULT_CATEGORY,
+    this.feature()?.properties?.['journeyDate'] ?? '',
+    this.feature()?.properties?.['tags'] ?? [],
+    this.feature()?.properties?.['thumbnail'] ?? DEFAULT_THUMBNAIL,
+  ));
 
   elementRef: ElementRef;
 
