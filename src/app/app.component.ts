413115f06d463a2b901e8b3cbea63be0062e25a3
Ram <ramachandrannellai@gmail.com>
2024-11-24 22:57:57 +0100

    feat: material ui based navigation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {SideNavbarComponent} from './component/side-navbar/side-navbar.component';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {LoadingSpinnerComponent} from "./component/loading-spinner/loading-spinner.component";
 import {ThemeToggleComponent} from "./component/theme-toggle/theme-toggle.component";
+import {NavigationMenuComponent} from "./component/navigation-menu/navigation-menu.component";
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {MatToolbar} from "@angular/material/toolbar";
+import {HOME_PAGE_INFO} from "./model/page.info.model";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [SideNavbarComponent, RouterOutlet, LoadingSpinnerComponent, ThemeToggleComponent],
+  imports: [RouterOutlet, LoadingSpinnerComponent, ThemeToggleComponent, NavigationMenuComponent, MatSidenavContainer, MatSidenavContent, MatSidenav, MatIcon, MatIconButton, MatToolbar, RouterLink, NgOptimizedImage],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'Journey';
+  protected readonly HOME_PAGE_INFO = HOME_PAGE_INFO;
 }
---
+++ src/app/component/navigation-menu/navigation-menu.component.ts
@@ -0,0 +1,49 @@
+import {Component, inject} from '@angular/core';
+import {
+    ABOUT_PAGE_INFO,
+    DASHBOARD_PAGE_INFO,
+    GALLERY_PAGE_INFO,
+    HOME_PAGE_INFO,
+    JOURNEY_SEARCH_PAGE_INFO,
+    LOGIN_PAGE_INFO,
+    LOGOUT_PAGE_INFO,
+    STATISTICS_PAGE_INFO,
+    TIMELINE_PAGE_INFO,
+    TOOLS_PAGE_INFO
+} from "../../model/page.info.model";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
+import {MatMenuModule} from "@angular/material/menu";
+import {NgIf} from "@angular/common";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconButton} from "@angular/material/button";
+import {AuthService} from "../../service/auth/auth.service";
+import {toSignal} from "@angular/core/rxjs-interop";
+
+@Component({
+    selector: 'app-navigation-menu',
+    standalone: true,
+    imports: [
+        MatSidenavModule, RouterLink, MatIconModule, MatListModule, RouterLinkActive, MatMenuModule, NgIf, MatToolbarModule, MatIconButton, RouterOutlet
+    ],
+    templateUrl: './navigation-menu.component.html',
+    styleUrl: './navigation-menu.component.scss'
+})
+export class NavigationMenuComponent {
+
+    protected readonly HOME_PAGE_INFO = HOME_PAGE_INFO;
+    protected readonly DASHBOARD_PAGE_INFO = DASHBOARD_PAGE_INFO;
+    protected readonly GALLERY_PAGE_INFO = GALLERY_PAGE_INFO;
+    protected readonly JOURNEY_SEARCH_PAGE_INFO = JOURNEY_SEARCH_PAGE_INFO;
+    protected readonly STATISTICS_PAGE_INFO = STATISTICS_PAGE_INFO;
+    protected readonly TIMELINE_PAGE_INFO = TIMELINE_PAGE_INFO;
+    protected readonly TOOLS_PAGE_INFO = TOOLS_PAGE_INFO;
+    protected readonly ABOUT_PAGE_INFO = ABOUT_PAGE_INFO;
+    protected readonly LOGIN_PAGE_INFO = LOGIN_PAGE_INFO;
+    protected readonly LOGOUT_PAGE_INFO = LOGOUT_PAGE_INFO;
+
+    authService = inject(AuthService);
+    isUserAuthenticated = toSignal(this.authService.isUserAuthenticatedAsObservable(), {initialValue: false});
+}
