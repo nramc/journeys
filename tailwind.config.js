8db489f77e92e1e41fe6fb8870dabf6b290ea9ae
Ram <ramachandrannellai@gmail.com>
2024-11-25 22:00:31 +0100

    feat: bootstrap css dependency removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3459E6',
+        secondary: '#952eab',
+        danger: '#dc3545',
+        success: '#198754',
+        warning: '#ffc107',
+        info: '#0dcaf0',
+        light: '#f8f9fa',
+        dark: '#212529',
+      }
+    },
   },
   plugins: [],
 }
