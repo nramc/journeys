0e23015d273ca22f8b69236fe3986d6f0172d687
Ram <ramachandrannellai@gmail.com>
2024-11-26 23:43:28 +0100

    feat: my account nav link moved to mat toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {LoadingSpinnerComponent} from "./component/loading-spinner/loading-spinner.component";
 import {ThemeToggleComponent} from "./component/theme-toggle/theme-toggle.component";
@@ -7,17 +7,27 @@ import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/mater
 import {MatIcon} from "@angular/material/icon";
 import {MatIconButton} from "@angular/material/button";
 import {MatToolbar} from "@angular/material/toolbar";
-import {HOME_PAGE_INFO} from "./model/page.info.model";
+import {HOME_PAGE_INFO, LOGOUT_PAGE_INFO} from "./model/page.info.model";
+import {MatListItem} from "@angular/material/list";
+import {NgIf} from "@angular/common";
+import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
+import {AuthService} from "./service/auth/auth.service";
+import {toSignal} from "@angular/core/rxjs-interop";
+import {MatTooltip} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet, LoadingSpinnerComponent, ThemeToggleComponent, NavigationMenuComponent, MatSidenavContainer, MatSidenavContent, MatSidenav, MatIcon, MatIconButton, MatToolbar, RouterLink],
+  imports: [RouterOutlet, LoadingSpinnerComponent, ThemeToggleComponent, NavigationMenuComponent, MatSidenavContainer, MatSidenavContent, MatSidenav, MatIcon, MatIconButton, MatToolbar, RouterLink, MatListItem, NgIf, MatMenu, MatMenuItem, MatMenuTrigger, MatTooltip],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'Journey';
   protected readonly HOME_PAGE_INFO = HOME_PAGE_INFO;
+  protected readonly LOGOUT_PAGE_INFO = LOGOUT_PAGE_INFO;
+
+  authService = inject(AuthService);
+  isUserAuthenticated = toSignal(this.authService.isUserAuthenticatedAsObservable(), {initialValue: false});
 }
