8a0023f67b6cc8c74ed07670fdd28bfbd4da9433
Ram <ramachandrannellai@gmail.com>
2024-11-30 23:02:06 +0100

    feat: home page styles migrated  with quick link section

--- src/app/page/home/quick-links/quick-links.component.ts
+++ src/app/page/home/quick-links/quick-links.component.ts
@@ -2,13 +2,15 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 import {RouterLink} from "@angular/router";
 import {NEW_JOURNEY_PAGE_INFO, TIMELINE_PAGE_INFO} from "../../../model/page.info.model";
+import {MatFabAnchor} from "@angular/material/button";
 
 @Component({
   selector: 'app-quick-links',
   standalone: true,
   imports: [
     MatIcon,
-    RouterLink
+    RouterLink,
+    MatFabAnchor
   ],
   templateUrl: './quick-links.component.html',
   styles: [],
