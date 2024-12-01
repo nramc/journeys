104d20520cc1320be0f97c815ccdc666b5481b96
Ram <ramachandrannellai@gmail.com>
2024-12-01 18:14:24 +0100

    feat: about page migrated

--- src/app/page/about/about.component.ts
+++ src/app/page/about/about.component.ts
@@ -2,16 +2,17 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {ABOUT_PAGE_INFO} from "../../model/page.info.model";
 import {NgOptimizedImage} from "@angular/common";
 import {Router} from "@angular/router";
-import {MatButton, MatFabAnchor, MatMiniFabAnchor, MatMiniFabButton} from "@angular/material/button";
+import {MatAnchor, MatButton, MatMiniFabAnchor} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
 import {MatIcon} from "@angular/material/icon";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    MatButton, MatCardModule, MatIcon, MatMiniFabButton, MatFabAnchor, MatMiniFabAnchor
+    MatButton, MatCardModule, MatIcon, MatMiniFabAnchor, FaIconComponent, MatAnchor
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
@@ -20,7 +21,7 @@ import {MatIcon} from "@angular/material/icon";
 export class AboutComponent {
   protected readonly ABOUT_PAGE_INFO = ABOUT_PAGE_INFO;
 
-  constructor(private router: Router) {
+  constructor(private readonly router: Router) {
   }
 
   signup() {
