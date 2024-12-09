f14f37cc56dc72705fe641c772c79aaaf26320af
Ram <ramachandrannellai@gmail.com>
2024-12-09 22:55:17 +0100

    feat: edit-journey-images-details.component.html migrated

--- src/app/page/journeys/edit-journey/edit-journey-image-details/edit-journey-image-item/edit-journey-image-item.component.ts
+++ src/app/page/journeys/edit-journey/edit-journey-image-details/edit-journey-image-item/edit-journey-image-item.component.ts
@@ -1,105 +1,26 @@
-import {ChangeDetectionStrategy, Component, input, output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, model, output} from '@angular/core';
 import {JourneyImageDetail} from "../../../../../model/core/journey.model";
 import {FormsModule} from "@angular/forms";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {JsonPipe, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-edit-journey-image-item',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+    NgIf,
+    JsonPipe
   ],
-  template: `
-    <div class="container">
-      <div class="model-header">
-        <h6 class="gradient-text m-1 border-bottom border-primary-subtle border-3 pb-2">Edit Image Details</h6>
-      </div>
-
-      <div [style]="'background-image: url('+imageItem().url+ ');background-size: cover;background-repeat: no-repeat;'">
-        <div class="d-flex flex-column flex-md-row justify-content-evenly align-items-center align-content-center"
-             [style]="'backdrop-filter: blur(15px);'">
-          <div class="image-thumbnail m-1 col-md-4 text-center">
-            <img
-              [src]="imageItem().url"
-              [alt]="imageItem().assetId"
-              [title]="imageItem().assetId"
-              class="img-thumbnail rounded border border-2 border-opacity-50 border-primary h-200-px w-200-px"
-              fill/>
-          </div>
-          <div class="image-details flex-fill m-1">
-
-            <form>
-              <div class="form-floating mb-1">
-                <input type="text" class="form-control ignore-highlight" id="imageUrl" name="imageUrl" placeholder="URL"
-                       [(ngModel)]="imageItem().url" readonly="readonly" disabled="disabled">
-                <label for="imageUrl">Image URL</label>
-              </div>
-              <div class="form-floating mb-1">
-                <input type="text" class="form-control ignore-highlight" id="imageId" name="imageId" placeholder="ID"
-                       [(ngModel)]="imageItem().assetId" readonly="readonly" disabled="disabled">
-                <label for="imageId">Asset ID</label>
-              </div>
-              <div class="form-floating mb-1">
-                <input type="text" class="form-control ignore-highlight" id="publicId" name="publicId" placeholder="ID"
-                       [(ngModel)]="imageItem().publicId" readonly="readonly" disabled="disabled">
-                <label for="publicId">Public ID</label>
-              </div>
-
-              <div class="form-floating mb-1">
-                <input type="text" class="form-control" id="title" name="title" placeholder="Title"
-                       [(ngModel)]="imageItem().title">
-                <label for="title">Title</label>
-              </div>
-
-              <div class="input-group mb-1">
-                <div class="input-group-text">
-                  <input class="form-check-input ignore-highlight" type="checkbox" id="favoriteFlag" name="favoriteFlag"
-                         [(ngModel)]="imageItem().isFavorite">
-                </div>
-                <input type="text" class="form-control" value="Make it as my favorite" disabled>
-              </div>
-
-              <!-- Journey Date -->
-              <div class="input-group input-group-sm mb-1">
-                <div class="form-floating">
-                  <input type="text" class="form-control form-control-sm ignore-highlight" id="eventDate"
-                         name="eventDate"
-                         [(ngModel)]="imageItem().eventDate"
-                         placeholder="yyyy-mm-dd" ngbDatepicker>
-                  <label for="eventDate">Event Date</label>
-                </div>
-                <div class="input-group-text">
-                  <button class="btn btn-sm btn-outline-secondary bi bi-calendar3 "
-                          type="button">&nbsp;
-                  </button>
-                </div>
-              </div>
-
-              <div class="input-group mb-1">
-                <div class="input-group-text">
-                  <input class="form-check-input ignore-highlight" type="checkbox" id="thumbnailFlag"
-                         name="thumbnailFlag" [(ngModel)]="imageItem().isThumbnail">
-                </div>
-                <input type="text" class="form-control" value="Make it as thumbnail" disabled>
-              </div>
-
-              <div class="d-inline-flex justify-content-center align-content-center align-items-center mt-2 w-100">
-                <button type="button" class="btn btn-primary me-1" (click)="save.emit(imageItem())">Save</button>
-                <button type="button" class="btn btn-danger me-1" (click)="remove.emit(imageItem())">Remove</button>
-                <button type="button" class="btn btn-dark" (click)="cancel.emit(imageItem())">Cancel</button>
-              </div>
-            </form>
-
-          </div>
-
-        </div>
-      </div>
-    </div>
-  `,
+  templateUrl: 'edit-journey-image-item.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EditJourneyImageItemComponent {
-  imageItem = input.required<JourneyImageDetail>();
+  imageItem = model<JourneyImageDetail>(new JourneyImageDetail("", ""));
   save = output<JourneyImageDetail>();
   cancel = output<JourneyImageDetail>();
   remove = output<JourneyImageDetail>();
