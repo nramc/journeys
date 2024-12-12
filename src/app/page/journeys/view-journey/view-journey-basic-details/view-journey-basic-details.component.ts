353c35a323a312c28ec6644286655cb46cd016fa
Ram <ramachandrannellai@gmail.com>
2024-12-12 21:17:10 +0100

    feat: view-journey.component

--- src/app/page/journeys/view-journey/view-journey-basic-details/view-journey-basic-details.component.ts
+++ src/app/page/journeys/view-journey/view-journey-basic-details/view-journey-basic-details.component.ts
@@ -1,6 +1,5 @@
 import {ChangeDetectionStrategy, Component, input} from '@angular/core';
 import {Journey} from "../../../../model/core/journey.model";
-import {DatePipe, NgForOf} from "@angular/common";
 import {MarkdownModule} from "ngx-markdown";
 import {DisplayMarkdownComponent} from "../../../../component/display-markdown-component/display-markdown.component";
 
@@ -8,13 +7,11 @@ import {DisplayMarkdownComponent} from "../../../../component/display-markdown-c
   selector: 'app-view-journey-basic-details',
   standalone: true,
   imports: [
-    DatePipe,
-    NgForOf,
     MarkdownModule,
     DisplayMarkdownComponent
   ],
   templateUrl: './view-journey-basic-details.component.html',
-  styleUrl: './view-journey-basic-details.component.scss',
+  styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ViewJourneyBasicDetailsComponent {
