3b836ca8da50a167106d25f5292ac0cf3c54294e
Ram <ramachandrannellai@gmail.com>
2024-12-13 19:52:55 +0100

    feat: my-totp-settings.component ui/ux improved

--- src/app/page/my-account/my-account-settings-page/my-totp-settings/totp-activation/totp-activation.component.ts
+++ src/app/page/my-account/my-account-settings-page/my-totp-settings/totp-activation/totp-activation.component.ts
@@ -1,44 +1,43 @@
-import {ChangeDetectionStrategy, Component, DestroyRef, inject, model, OnInit} from '@angular/core';
-import {MatDialogClose, MatDialogRef} from "@angular/material/dialog";
+import {ChangeDetectionStrategy, Component, DestroyRef, inject} from '@angular/core';
+import {MatDialogRef} from "@angular/material/dialog";
 import {MyAccountService} from "../../../../../service/my-account/my-account.service";
-import {QrCodeData} from "../../../../../model/account/qr-code-data";
-import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
-import {FormsModule} from "@angular/forms";
+import {takeUntilDestroyed, toSignal} from "@angular/core/rxjs-interop";
+import {FormsModule, NgModel} from "@angular/forms";
 import {TotpActivation} from "../../../../../service/my-account/totp-activation";
 import {NotificationService} from "../../../../../service/common/notification.service";
-import {MatProgressSpinner} from "@angular/material/progress-spinner";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatButtonModule} from "@angular/material/button";
+import {NgIf} from "@angular/common";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
   selector: 'app-totp-activation',
   standalone: true,
   imports: [
-    MatDialogClose,
     FormsModule,
-    MatProgressSpinner
+    MatProgressSpinnerModule,
+    MatExpansionModule,
+    MatButtonModule,
+    NgIf,
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './totp-activation.component.html',
   styleUrl: './totp-activation.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TotpActivationComponent implements OnInit {
+export class TotpActivationComponent {
   private readonly destroyRef = inject(DestroyRef);
   private readonly dialogRef: MatDialogRef<TotpActivationComponent> = inject(MatDialogRef<TotpActivationComponent>);
   private readonly myAccountService = inject(MyAccountService);
   private readonly notificationService = inject(NotificationService);
 
-  qrCodeData = model<QrCodeData>();
+  qrCodeData = toSignal(this.myAccountService.generateNewQRCode());
 
-  ngOnInit(): void {
-    this.myAccountService.generateNewQRCode()
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe({
-        next: data => this.qrCodeData.set(data),
-        error: err => console.log(err)
-      });
-  }
-
-  activate(totpCode: HTMLInputElement) {
-    if (totpCode.validity.valid && this.qrCodeData()?.secretKey) {
+  activate(totpCode: NgModel) {
+    if (totpCode.valid && this.qrCodeData()?.secretKey) {
       // activate totp now
       const activationRequest: TotpActivation = {secretKey: this.qrCodeData()!.secretKey, code: totpCode.value};
       this.myAccountService.activateTotp(activationRequest)
@@ -56,4 +55,7 @@ export class TotpActivationComponent implements OnInit {
   }
 
 
+  cancelActivation() {
+    this.dialogRef.close(false);
+  }
 }
