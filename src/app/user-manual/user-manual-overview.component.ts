43d000045ad44737fa8319ac96d82f65b61f1b85
Ram <ramachandrannellai@gmail.com>
2025-04-16 22:10:52 +0200

    feat: integrate FullCalendar into User Manual overview component

--- src/app/user-manual/user-manual-overview.component.ts
+++ src/app/user-manual/user-manual-overview.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CalendarOptions} from "@fullcalendar/core";
+import {CommonModule} from "@angular/common";
+import {FullCalendarModule} from "@fullcalendar/angular";
+import dayGridPlugin from '@fullcalendar/daygrid';
+
 
 @Component({
   selector: 'app-user-manual-overview',
-  imports: [],
-  template: '<h1>User Manual: Overview</h1>',
+  imports: [CommonModule, FullCalendarModule],
+  template: '<full-calendar [options]="calendarOptions"></full-calendar>',
   styles: []
 })
 export class UserManualOverviewComponent {
+  calendarOptions: CalendarOptions = {
+    headerToolbar: {
+      right: 'today prev,next',
+      center: 'title',
+      left: 'dayGridMonth,dayGridWeek,dayGridDay'
+    },
+    height: 'auto',
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin]
+  };
 
 }
