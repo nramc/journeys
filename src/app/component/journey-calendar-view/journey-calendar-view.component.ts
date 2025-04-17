0ed167e30479840f481743febec700afe14e306f
Ram <ramachandrannellai@gmail.com>
2025-04-17 17:24:24 +0200

    feat: update journey calendar view to fetch events from JourneyService

--- src/app/component/journey-calendar-view/journey-calendar-view.component.ts
+++ src/app/component/journey-calendar-view/journey-calendar-view.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {CalendarOptions, EventInput, EventSourceInput} from "@fullcalendar/core";
 import rrulePlugin from "@fullcalendar/rrule";
 import multiMonthPlugin from '@fullcalendar/multimonth'
 import dayGridPlugin from "@fullcalendar/daygrid";
 import {FullCalendarModule} from "@fullcalendar/angular";
 import {CommonModule} from "@angular/common";
+import {JourneyService} from "../../service/journey/journey.service";
+import {toSignal} from "@angular/core/rxjs-interop";
+import {FeatureCollection} from "geojson";
+import {map} from "rxjs";
 
 @Component({
   selector: 'app-journey-calendar-view',
@@ -13,29 +17,16 @@ import {CommonModule} from "@angular/common";
   styleUrl: './journey-calendar-view.component.scss'
 })
 export class JourneyCalendarViewComponent {
-
-  defaultEventProperties : EventInput = {
+  private readonly journeyService = inject(JourneyService);
+  defaultEventProperties: EventInput = {
     rrule: {
       freq: 'yearly',
     }
   };
 
-  journeys: EventSourceInput = [
-    {
-      id: '550e8400-e29b-41d4-a716-446655440000',
-      title: 'New Journey which has length maximum of 50 characters',
-      url: 'https://www.google.com',
-      start: '2025-04-17',
-      ...this.defaultEventProperties
-    },
-    {
-      id: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
-      title: 'Another Journey which has length maximum of 50 characters',
-      url: 'https://www.google.com',
-      start: '2025-04-17',
-      ...this.defaultEventProperties
-    }
-  ];
+  journeys = toSignal(this.journeyService.getAllJourneysAsGeoJson()
+    .pipe(map(data => this.toEventData(data))), {initialValue: [] as EventSourceInput});
+
   calendarOptions: CalendarOptions = {
     headerToolbar: {
       right: 'today prev,next',
@@ -43,11 +34,11 @@ export class JourneyCalendarViewComponent {
       left: 'multiMonthYear,dayGridMonth,dayGridWeek,dayGridDay'
     },
     height: 'auto',
-    initialView: 'multiMonthYear',
+    initialView: 'dayGridMonth',
     plugins: [rrulePlugin, multiMonthPlugin, dayGridPlugin],
     editable: false,
     eventDisplay: 'block',
-    events: this.journeys,
+    events: [],
     defaultAllDay: true,
     navLinks: true,
     navLinkDayClick: function (date, jsEvent) {
@@ -57,4 +48,15 @@ export class JourneyCalendarViewComponent {
       info.el.setAttribute('title', info.event.title);
     }
   };
+
+  private toEventData(featureCollection: FeatureCollection) {
+    return featureCollection?.features.map(feature => ({
+      id: feature.properties?.['id'],
+      title: feature.properties?.['name'],
+      url: feature.properties?.['thumbnail'],
+      start: feature.properties?.['journeyDate'],
+      ...this.defaultEventProperties
+    })) as EventSourceInput;
+  }
+
 }
