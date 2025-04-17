1b0c15ae4fb9ed0ff0021747263b4c85d222e2b2
Ram <ramachandrannellai@gmail.com>
2025-04-18 00:12:52 +0200

    feat: update journey calendar view component to enhance event data handling

--- src/app/component/journey-calendar-view/journey-calendar-view.component.ts
+++ src/app/component/journey-calendar-view/journey-calendar-view.component.ts
@@ -1,5 +1,5 @@
 import {Component, inject} from '@angular/core';
-import {CalendarOptions, EventInput, EventSourceInput} from "@fullcalendar/core";
+import {CalendarOptions, EventSourceInput} from "@fullcalendar/core";
 import rrulePlugin from "@fullcalendar/rrule";
 import multiMonthPlugin from '@fullcalendar/multimonth'
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -12,56 +12,54 @@ import {map} from "rxjs";
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-journey-calendar-view',
-  imports: [CommonModule, FullCalendarModule],
-  template: `
-    <full-calendar [options]="calendarOptions" [events]="journeys()"></full-calendar>`,
-  styles: []
+    selector: 'app-journey-calendar-view',
+    imports: [CommonModule, FullCalendarModule],
+    template: `
+        <full-calendar [options]="calendarOptions" [events]="journeys()"></full-calendar>`,
+    styles: []
 })
 export class JourneyCalendarViewComponent {
-  private readonly journeyService = inject(JourneyService);
-  private readonly router = inject(Router);
-  defaultEventProperties: EventInput = {
-    rrule: {
-      freq: 'yearly',
+    private readonly journeyService = inject(JourneyService);
+    private readonly router = inject(Router);
+
+    journeys = toSignal(this.journeyService.getAllJourneysAsGeoJson()
+        .pipe(map(data => this.toEventData(data))), {initialValue: [] as EventSourceInput});
+
+    calendarOptions: CalendarOptions = {
+        headerToolbar: {
+            right: 'today prev,next',
+            center: 'title',
+            left: 'multiMonthYear,dayGridMonth,dayGridWeek,dayGridDay'
+        },
+        height: 'auto',
+        initialView: 'dayGridMonth',
+        plugins: [rrulePlugin, multiMonthPlugin, dayGridPlugin],
+        editable: false,
+        eventDisplay: 'block',
+        events: this.journeys(),
+        navLinks: true,
+        navLinkDayClick: function (date) {
+            console.log('day', date.toISOString());
+        },
+        eventDidMount: info => {
+            info.el.setAttribute('title', info.event.title);
+        },
+        eventClick: info => {
+            this.router.navigate(['/journey', info.event.id, 'view']).then(console.log);
+        }
+    };
+
+    private toEventData(featureCollection: FeatureCollection) {
+        return featureCollection?.features.map(feature => ({
+            id: feature.id,
+            title: feature.properties?.['name'],
+            extendedProps: feature.properties,
+            allDay: true,
+            rrule: {
+                freq: 'yearly',
+                dtstart: feature.properties?.['journeyDate']
+            }
+        })) as EventSourceInput;
     }
-  };
-
-  journeys = toSignal(this.journeyService.getAllJourneysAsGeoJson()
-    .pipe(map(data => this.toEventData(data))), {initialValue: [] as EventSourceInput});
-
-  calendarOptions: CalendarOptions = {
-    headerToolbar: {
-      right: 'today prev,next',
-      center: 'title',
-      left: 'multiMonthYear,dayGridMonth,dayGridWeek,dayGridDay'
-    },
-    height: 'auto',
-    initialView: 'dayGridMonth',
-    plugins: [rrulePlugin, multiMonthPlugin, dayGridPlugin],
-    editable: false,
-    eventDisplay: 'block',
-    events: this.journeys(),
-    defaultAllDay: true,
-    navLinks: true,
-    navLinkDayClick: function (date) {
-      console.log('day', date.toISOString());
-    },
-    eventDidMount: info => {
-      info.el.setAttribute('title', info.event.title);
-    },
-    eventClick: info => {
-      this.router.navigate(['/journey', info.event.id, 'view']).then(console.log);
-    }
-  };
-
-  private toEventData(featureCollection: FeatureCollection) {
-    return featureCollection?.features.map(feature => ({
-      id: feature.id,
-      title: feature.properties?.['name'],
-      start: feature.properties?.['journeyDate'],
-      ...this.defaultEventProperties
-    })) as EventSourceInput;
-  }
 
 }
