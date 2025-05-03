0aecaf02fe201ae74576ba3502ef99704d6c98b8
Ram <ramachandrannellai@gmail.com>
2025-05-03 22:05:36 +0200

    feat: add journey manager component to user manual

---
+++ src/app/user-manual/manual-journey-manager/manual-journey-manager.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-journey-manager',
+  imports: [],
+  templateUrl: './manual-journey-manager.component.html',
+  styles: []
+})
+export class ManualJourneyManagerComponent {
+
+}
