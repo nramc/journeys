c6a85ca1cae48c1a1017c100769d8050baa225b3
Ram <ramachandrannellai@gmail.com>
2025-05-04 17:47:10 +0200

    feat: add gallery section to user manual and enhance link accessibility

---
+++ src/app/user-manual/manual-gallery/manual-gallery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-gallery',
+  imports: [],
+  templateUrl: './manual-gallery.component.html',
+  styles: []
+})
+export class ManualGalleryComponent {
+
+}
