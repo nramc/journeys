66365964798ab9192c927d1fbbe2bbccd1756d57
Ram <ramachandrannellai@gmail.com>
2025-05-07 20:56:07 +0200

    feat: add 'My Account' section to user manual for improved navigation

---
+++ src/app/user-manual/manual-my-account/manual-my-account.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-my-account',
+  imports: [],
+  templateUrl: './manual-my-account.component.html',
+  styles: []
+})
+export class ManualMyAccountComponent {
+
+}
