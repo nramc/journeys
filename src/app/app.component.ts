5ebcc068da7df59be2dae451e007b6361faa08cb
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2025-05-31 22:15:18 +0200

    refactor: remove hardcoded passkey registration and login methods from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,9 @@ import {NavigationMenuComponent} from "./component/navigation-menu/navigation-me
 import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
 import {ToolbarComponent} from "./component/toolbar/toolbar.component";
 import {BreakpointObserver} from "@angular/cdk/layout";
-import {from, map, switchMap, tap} from "rxjs";
+import {map} from "rxjs";
 import {toSignal} from "@angular/core/rxjs-interop";
 import {FormsModule} from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
-import * as webauthnJson from "@github/webauthn-json";
-import {CredentialRequestOptionsJSON} from "@github/webauthn-json";
-import {PublicKeyCredentialCreationOptionsJSON} from "@github/webauthn-json/src/webauthn-json/basic/json";
 
 
 @Component({
@@ -28,48 +24,5 @@ export class AppComponent {
     this.observer.observe(['(max-width: 768px)']).pipe(map((res) => res.matches)),
     {initialValue: false}
   );
-
-  // testing
-  username = 'ramachandrannellai@gmail.com';
-  http = inject(HttpClient);
-
-  async registerStart() {
-    const authHeader = 'Basic ' + btoa(`${this.username}:RamNellai@7`);
-    const headers = {headers: {'Authorization': authHeader}};
-
-    this.http.post<PublicKeyCredentialCreationOptionsJSON>(
-      'https://localhost:8080/webauthn/register/start', {username: this.username}, headers)
-      .pipe(
-        tap(res => console.log('Credential options received:', res)),
-        switchMap((credentialCreateOptions) =>
-          from(webauthnJson.create({publicKey: credentialCreateOptions})).pipe(
-            tap(attestation => console.log('Attestation object:', attestation)),
-            switchMap((attestation) =>
-              this.http.post<boolean>('https://localhost:8080/webauthn/register/finish', attestation, headers)
-            )
-          )
-        )
-      ).subscribe({
-      next: (result) => {
-        console.log('Registration completed successfully:', result);
-      },
-      error: (err) => {
-        console.error('Error during registration:', err);
-      }
-    });
-  }
-
-  async loginStart() {
-    const response = await this.http.post<CredentialRequestOptionsJSON>(
-      'http://localhost:8080/webauthn/authenticate/options', {username: this.username},
-      {headers: {'Authorization': 'Basic ' + btoa(this.username + ':' + 'RamNellai@7')}}
-    ).toPromise();
-
-    const assertion = await webauthnJson.get(response!);
-    await this.http.post('http://localhost:8080/login/webauthn', assertion,
-      {headers: {'Authorization': 'Basic ' + btoa(this.username + ':' + 'RamNellai@7')}}
-    ).toPromise();
-    console.log('Login successful');
-  }
 
 }
