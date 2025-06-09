fedced89881feb1a3cff3b1c0c04a44351be0dfe
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2025-06-09 13:45:41 +0200

    feat: add text-to-speech functionality to enhance content engagement

--- src/app/component/display-markdown-component/display-markdown.component.ts
+++ src/app/component/display-markdown-component/display-markdown.component.ts
@@ -1,19 +1,34 @@
-import {ChangeDetectionStrategy, Component, input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, input, ViewChild} from '@angular/core';
 import {MarkdownComponent} from "ngx-markdown";
 import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
-    selector: 'app-display-markdown-component',
-    imports: [
-        MarkdownComponent,
-        MatCardModule
-    ],
-    templateUrl: './display-markdown.component.html',
-    styles: [],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-display-markdown-component',
+  imports: [
+    MarkdownComponent,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './display-markdown.component.html',
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DisplayMarkdownComponent {
   title = input<string>('');
   markdownText = input<string>('');
+  @ViewChild('markdownComponent', {read: ElementRef}) markdownElementRef!: ElementRef;
 
+
+  readContent() {
+    const element = this.markdownElementRef.nativeElement;
+    const text = (element.innerText ?? element.textContent) ?? '';
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'en-US';
+    utterance.rate = 0.65;
+    utterance.pitch = 0;
+    window.speechSynthesis.speak(utterance);
+  }
 }
