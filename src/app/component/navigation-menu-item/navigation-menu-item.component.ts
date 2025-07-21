5dce935c9c87698bd240ae9e7580a3d13b830c43
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2025-07-21 22:51:04 +0200

    feat: implement navigation menu with icons for improved user experience

--- src/app/component/navigation-menu-item/navigation-menu-item.component.ts
+++ src/app/component/navigation-menu-item/navigation-menu-item.component.ts
@@ -2,10 +2,11 @@ import {Component, input} from '@angular/core';
 import {MatIconModule} from "@angular/material/icon";
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {PageInfo} from "../../model/page.info.model";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-navigation-menu-item',
-  imports: [MatIconModule, RouterLink, RouterLinkActive],
+  imports: [MatIconModule, RouterLink, RouterLinkActive, MatTooltipModule],
   templateUrl: './navigation-menu-item.component.html',
   styles: []
 })
