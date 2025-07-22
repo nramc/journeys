115a365201251c8d2cd4705f9c9b01a8bee81e0d
nramc <ramachandrannellai@gmail.com>
2025-07-22 19:33:30 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/nramc/journeys/commit/0e35a515f9e7d37026ab301a4dfa7e6213436ecc

---
+++ chunk-ZUEX2YVV.js
@@ -0,0 +1,1 @@
+import{I as i}from"./chunk-Z5EQZODQ.js";import{la as e,yb as t}from"./chunk-JDJNICZL.js";var m=(()=>{class o{static \u0275fac=function(r){return new(r||o)};static \u0275mod=t({type:o});static \u0275inj=e({imports:[i]})}return o})();export{m as a};
