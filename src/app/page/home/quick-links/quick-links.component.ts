df3f3c818d582f04fb0a7d6f55a29aefea44151f
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2025-07-22 21:15:19 +0200

    feat: update quick links component to use directive for authentication check

--- src/app/page/home/quick-links/quick-links.component.ts
+++ src/app/page/home/quick-links/quick-links.component.ts
@@ -1,26 +1,27 @@
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
-import {MatIcon} from "@angular/material/icon";
+import {MatIconModule} from "@angular/material/icon";
 import {RouterLink} from "@angular/router";
 import {NEW_JOURNEY_PAGE_INFO, TIMELINE_PAGE_INFO} from "../../../model/page.info.model";
-import {MatFabAnchor} from "@angular/material/button";
+import {MatButtonModule} from "@angular/material/button";
 import {AuthService} from "../../../service/auth/auth.service";
-import {NgIf} from "@angular/common";
+import {HasAuthenticatedDirective} from "../../../directive/has-authenticated.directive";
+
 
 @Component({
-    selector: 'app-quick-links',
-    imports: [
-        MatIcon,
-        RouterLink,
-        MatFabAnchor,
-        NgIf
-    ],
-    templateUrl: './quick-links.component.html',
-    styles: [],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-quick-links',
+  imports: [
+    MatIconModule,
+    RouterLink,
+    MatButtonModule,
+    HasAuthenticatedDirective
+  ],
+  templateUrl: './quick-links.component.html',
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class QuickLinksComponent {
   protected readonly NEW_JOURNEY_PAGE_INFO = NEW_JOURNEY_PAGE_INFO;
   protected readonly TIMELINE_PAGE_INFO = TIMELINE_PAGE_INFO;
 
-  protected readonly authService =  inject(AuthService);
+  protected readonly authService = inject(AuthService);
 }
