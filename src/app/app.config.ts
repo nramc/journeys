162eb244ff68297e164d60e1b45fa789d4fd1d1e
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2026-01-30 20:38:47 +0100

    fear: ng 21 update (#1139)
    * chore: update Angular dependencies to version 21.1.2 and adjust TypeScript configuration
    
    * refactor: update color themes and icon styles for consistency
    
    * chore: update Angular dependencies to version 21.1.2
    
    * chore: remove unused animations provider from app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,6 @@ import {MatIconModule} from "@angular/material/icon";
 import {LIGHTBOX_CONFIG, LightboxConfig} from "ng-gallery/lightbox";
 import {CustomErrorHandler} from "./utility/handler/error.handler";
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
-import {provideAnimations} from "@angular/platform-browser/animations";
 import {provideRouter, withComponentInputBinding, withInMemoryScrolling, withViewTransitions} from "@angular/router";
 import {ROUTES} from "./app.routes";
 import {loadingInterceptor} from "./utility/handler/loading.interceptor";
@@ -25,7 +24,6 @@ export const appConfig: ApplicationConfig = {
     },
     {provide: ErrorHandler, useClass: CustomErrorHandler},
     provideHttpClient(withInterceptors([loadingInterceptor])),
-    provideAnimations(),
     provideRouter(ROUTES,
       withComponentInputBinding(),
       withViewTransitions(),
