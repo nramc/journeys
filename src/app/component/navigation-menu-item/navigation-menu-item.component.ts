2e0e14fe7fe7c9ddf07ac4fd94d889ffe6604047
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2026-05-25 20:23:57 +0200

    fix: resolve sidenav start position error and enhance navigation menu responsiveness

--- src/app/component/navigation-menu-item/navigation-menu-item.component.ts
+++ src/app/component/navigation-menu-item/navigation-menu-item.component.ts
@@ -1,17 +1,16 @@
-import {Component, input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input} from '@angular/core';
 import {MatIconModule} from "@angular/material/icon";
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {PageInfo} from "../../model/page.info.model";
-import {MatTooltipModule} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-navigation-menu-item',
-  imports: [MatIconModule, RouterLink, RouterLinkActive, MatTooltipModule],
+  imports: [MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './navigation-menu-item.component.html',
-  styles: []
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavigationMenuItemComponent {
   showLabel = input<boolean>(true);
   pageInfo = input.required<PageInfo>()
-
 }
